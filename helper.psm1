9e9b36080d319fb07212265864a8de68a2d417ef
Wilfried Goesgens <willi@arangodb.com>
2018-11-06 13:22:10 +0100

    fix brackets

--- helper.psm1
+++ helper.psm1
@@ -871,10 +871,9 @@ Function launchTest($which) {
     $global:launcheableTests[$which]['pid'] = $process.Id
     $global:launcheableTests[$which]['launchDate'] = $((Get-Date).ToUniversalTime().ToString('yyyy-MM-ddTHH.mm.ssZ'))
 
-    $str=$test | where {($_.Name -ne "commandline")} | Out-String
+    $str=$($test | where {($_.Name -ne "commandline")} | Out-String)
     Write-Host $str
     Pop-Location
-
 }
 
 Function registerTest($testname, $index, $bucket, $filter, $moreParams, $cluster, $weight)
@@ -1017,7 +1016,7 @@ Function LaunchController($seconds)
                 Else {
                     $currentScore = $currentScore - $test['weight']
                     Write-Host "$((Get-Date).ToUniversalTime().ToString('yyyy-MM-ddTHH.mm.ssZ')) Testrun finished: "$test['identifier'] $test['launchdate']
-                    $str=$test | where {($_.Name -ne "commandline")} | Out-String
+                    $str=$($test | where {($_.Name -ne "commandline")} | Out-String)
                     $test['pid'] = -1
                 }
             }
@@ -1036,7 +1035,7 @@ Function LaunchController($seconds)
         ForEach ($test in $global:launcheableTests) {
             if ($test['pid'] -gt 0) {
               Write-Host "Testrun timeout:"
-              $str=$test | where {($_.Name -ne "commandline")} | Out-String
+              $str=$($test | where {($_.Name -ne "commandline")} | Out-String)
               Write-Host $str
               ForEach ($childProcesses in $(Get-WmiObject win32_process | Where {$_.ParentProcessId -eq $test['pid']})) {
                 ForEach ($childChildProcesses in $(Get-WmiObject win32_process | Where {$_.ParentProcessId -eq $test['pid']})) {
