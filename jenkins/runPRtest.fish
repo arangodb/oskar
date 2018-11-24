576a52fc744e7895a5499712c14775ada6d35cc9
Frank Celler <frank@arangodb.com>
2018-11-24 12:57:22 +0100

    added PARALLELISM_PR_TEST

--- jenkins/runPRtest.fish
+++ jenkins/runPRtest.fish
@@ -5,11 +5,11 @@ lockDirectory ; updateOskar ; clearResults
 
 eval $EDITION ; eval $STORAGE_ENGINE ; eval $TEST_SUITE ; skipGrey
 
-if test -z "$PARALLELISM_FULL_TEST"
-  set -g PARALLELISM_FULL_TEST 20
+if test -z "$PARALLELISM_PR_TEST"
+  set -g PARALLELISM_PR_TEST 20
 end
 
-parallelism "$PARALLELISM_FULL_TEST"
+parallelism "$PARALLELISM_PR_TEST"
 
 switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
 and oskar1
--- jenkins/runQuickyTestMac.fish
+++ jenkins/runQuickyTestMac.fish
@@ -16,11 +16,11 @@ or begin
   exit 1
 end
 
-if test -z "$PARALLELISM_FULL_TEST"
-  set -g PARALLELISM_FULL_TEST 20
+if test -z "$PARALLELISM_QUICKY_TEST"
+  set -g PARALLELISM_QUICKY_TEST 20
 end
 
-parallelism "$PARALLELISM_FULL_TEST"
+parallelism "$PARALLELISM_QUICKY_TEST"
 
 enterprise ; rocksdb ; cluster ; skipGrey
 
