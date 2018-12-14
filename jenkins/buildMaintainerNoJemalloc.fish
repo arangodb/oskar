76ed56a445f3ae4c79856e08ea6bdae79fb237c2
Max Neunhoeffer <max@arangodb.com>
2018-12-14 23:45:29 +0100

    Add a jenkins compilation script without jemalloc.

---
+++ jenkins/buildMaintainerNoJemalloc.fish
@@ -0,0 +1,24 @@
+#!/usr/bin/env fish
+source jenkins/helper.jenkins.fish ; prepareOskar
+
+lockDirectory ; updateOskar ; clearResults
+rocksdb ; cluster ; maintainerOn ; jemallocOff
+
+eval $EDITION
+
+echo "--------------------------------------------------------------------------------"
+showConfig
+
+echo Working on branch $ARANGODB_BRANCH of main repository and
+echo on branch $ENTERPRISE_BRANCH of enterprise repository.
+
+switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
+and buildStaticArangoDB
+
+set -l s $status
+if test $s -ne 0
+  echo Build failure with maintainer mode on in community.
+end
+cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory 
+exit $s
+
