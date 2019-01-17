98703944e7e867981df6700c4872cec3490647de
Frank Celler <frank@arangodb.com>
2019-01-17 20:26:29 +0100

    added releaseRepositories.fish

--- jenkins/releaseSource.fish
+++ jenkins/releaseSource.fish
@@ -1,4 +1,9 @@
 #!/usr/bin/env fish
+if test -z "$RELEASE_TAG"
+  echo "RELEASE_TAG required"
+  exit 1
+end
+
 source jenkins/helper.jenkins.fish ; prepareOskar
 
 lockDirectory ; updateOskar ; clearResults ; cleanWorkspace
