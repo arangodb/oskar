56c34e1aa2824d5bca274d2b22083c940e900864
Frank Celler <frank@arangodb.com>
2019-01-17 17:31:01 +0100

    changed download directory

--- jenkins/releasePreparePublish.fish
+++ jenkins/releasePreparePublish.fish
@@ -1,4 +1,16 @@
 #!/usr/bin/env fish
+if test (count $argv) -lt 1
+  echo usage: (status current-filename) "<destination>"
+  exit 1
+end
+
+if test -z "$RELEASE_TAG"
+  echo "RELEASE_TAG required"
+  exit 1
+end
+
+umask 000
+
 source jenkins/helper.jenkins.fish ; prepareOskar
 
 lockDirectory ; updateOskar ; clearResults ; cleanWorkspace
@@ -6,10 +18,13 @@ lockDirectory ; updateOskar ; clearResults ; cleanWorkspace
 switchBranches "$RELEASE_TAG" "$RELEASE_TAG" true
 and findArangoDBVersion
 
-and set -g SP_PACKAGES $STORAGE_PATH/$ARANGODB_PACKAGES
-and set -g SP_SNIPPETS_CO $STORAGE_PATH/$ARANGODB_PACKAGES/snippets/Community
-and set -g SP_SNIPPETS_EN $STORAGE_PATH/$ARANGODB_PACKAGES/snippets/Enterprise
-and set -g SP_SOURCE $STORAGE_PATH/$ARANGODB_PACKAGES/source
+set -xg SRC $argv[1]/stage1/${RELEASE_TAG}
+set -xg DST $argv[1]/stage2/$ARANGODB_PACKAGES
+
+and set -g SP_PACKAGES $DST
+and set -g SP_SNIPPETS_CO $DST/snippets/Community
+and set -g SP_SNIPPETS_EN $DST/snippets/Enterprise
+and set -g SP_SOURCE $DST/source
 and set -g WS_PACKAGES $WORKSPACE/release/packages
 and set -g WS_SNIPPETS $WORKSPACE/release/snippets
 and set -g WS_SOURCE $WORKSPACE/release/source
@@ -20,17 +35,17 @@ and echo "checking snippets source directory '$WS_SNIPPETS'"
 and test -d $WS_SNIPPETS
 and echo "checking source source directory '$WS_SOURCE'"
 and test -d $WS_SOURCE
-and echo "creating packages destination directory '$SP_PACKAGES'"
-and mkdir -p $SP_PACKAGES
-and echo "creating snippets destination directory '$SP_SNIPPETS_CO'"
+and echo "creating destination directory '$DST'"
+and mkdir -p $DST
+and echo "creating community snippets destination directory '$SP_SNIPPETS_CO'"
 and mkdir -p $SP_SNIPPETS_CO
-and echo "creating snippets destination directory '$SP_SNIPPETS_EN'"
+and echo "creating enterprise snippets destination directory '$SP_SNIPPETS_EN'"
 and mkdir -p $SP_SNIPPETS_EN
 and echo "creating source destination directory '$SP_SOURCE'"
 and mkdir -p $SP_SOURCE
 
 and echo "========== COPYING PACKAGES =========="
-and tar -C $WORKSPACE/release -c -f - packages | tar -C $SP_PACKAGES -x -v -f -
+and tar -C $WORKSPACE/release -c -f - packages | tar -C $DST -x -v -f -
 and echo "========== COPYING SOURCE =========="
 and tar -C $WS_SOURCE -c -f - . | tar -C $SP_SOURCE -x -v -f -
 and echo "========== COPYING SNIPPETS =========="
