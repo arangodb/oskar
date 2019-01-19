e6d7fb7b9bbd23b32ebf58b27dfa923bb937e400
Frank Celler <frank@arangodb.com>
2019-01-19 20:41:09 +0100

    added pushing

--- jenkins/releaseUpdateGithub.fish
+++ jenkins/releaseUpdateGithub.fish
@@ -22,8 +22,10 @@ source jenkins/helper.jenkins.fish ; prepareOskar
 
 function updateRepository
   set -l cid (git rev-parse HEAD)
+  and git reset --hard
   and git fetch origin $GIT_BRANCH
   and git checkout $GIT_BRANCH
+  and git clean -fdx
   and git reset --hard $cid
   and echo "FORCING UPDATE $GIT_BRANCH"
   and git push --force origin $GIT_BRANCH
