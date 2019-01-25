664f0463e53f69d8bd4c76402e30041c4ea4359e
Frank Celler <frank@arangodb.com>
2019-01-25 14:46:15 +0100

    try upload 4 times

--- jenkins/releaseUploadFiles.fish
+++ jenkins/releaseUploadFiles.fish
@@ -15,14 +15,24 @@ lockDirectory ; updateOskar ; clearResults ; cleanWorkspace
 
 switchBranches "$RELEASE_TAG" "$RELEASE_TAG" true
 and findArangoDBVersion
-and cd /mnt/buildfiles/stage2
-and gsutil rsync -r 3.4/source gs://download.arangodb.com/Source
-and gsutil rsync -r 3.4/packages/Community gs://download.arangodb.com/$ARANGODB_REPO/Community
-and gsutil rsync -r 3.4/repositories/Community/Debian gs://download.arangodb.com/$ARANGODB_REPO/DEBIAN
-and gsutil rsync -r 3.4/repositories/Community/RPM gs://download.arangodb.com/$ARANGODB_REPO/RPM
-and gsutil rsync -r 3.4/packages/Enterprise gs://download.arangodb.com/$ENTERPRISE_DOWNLOAD_KEY/$ARANGODB_REPO/Enterprise
-and gsutil rsync -r 3.4/repositories/Enterprise/Debian gs://download.arangodb.com/$ENTERPRISE_DOWNLOAD_KEY/$ARANGODB_REPO/DEBIAN
-and gsutil rsync -r 3.4/repositories/Enterprise/RPM gs://download.arangodb.com/$ENTERPRISE_DOWNLOAD_KEY/$ARANGODB_REPO/RPM
+or begin unlockDirectory ; exit 1 ; end
+
+function upload
+  cd /mnt/buildfiles/stage2
+  and gsutil rsync -r 3.4/source gs://download.arangodb.com/Source
+  and gsutil rsync -r 3.4/packages/Community gs://download.arangodb.com/$ARANGODB_REPO/Community
+  and gsutil rsync -r 3.4/repositories/Community/Debian gs://download.arangodb.com/$ARANGODB_REPO/DEBIAN
+  and gsutil rsync -r 3.4/repositories/Community/RPM gs://download.arangodb.com/$ARANGODB_REPO/RPM
+  and gsutil rsync -r 3.4/packages/Enterprise gs://download.arangodb.com/$ENTERPRISE_DOWNLOAD_KEY/$ARANGODB_REPO/Enterprise
+  and gsutil rsync -r 3.4/repositories/Enterprise/Debian gs://download.arangodb.com/$ENTERPRISE_DOWNLOAD_KEY/$ARANGODB_REPO/DEBIAN
+  and gsutil rsync -r 3.4/repositories/Enterprise/RPM gs://download.arangodb.com/$ENTERPRISE_DOWNLOAD_KEY/$ARANGODB_REPO/RPM
+end
+
+# there might be internet hickups
+upload
+or upload
+or upload
+or upload
 
 set -l s $status
 unlockDirectory
