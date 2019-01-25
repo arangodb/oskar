3d598bf1622f93191a15636c086b2b830f13d378
Frank Celler <frank@arangodb.com>
2019-01-25 15:20:49 +0100

    upload snippets

---
+++ jenkins/releaseUploadSnippets.fish
@@ -0,0 +1,38 @@
+#!/usr/bin/env fish
+if test -z "$RELEASE_TAG"
+  echo "RELEASE_TAG required"
+  exit 1
+end
+
+if test "$RELEASE_TYPE" = "preview"
+  echo "building an preview, not updating snippets"
+  exit 0
+end
+
+if test "$RELEASE_IS_HEAD" != "true"
+  echo "building an older release, not updating snippets"
+  exit 0
+end
+
+source jenkins/helper.jenkins.fish ; prepareOskar
+
+lockDirectory ; updateOskar ; clearResults ; cleanWorkspace
+
+switchBranches "$RELEASE_TAG" "$RELEASE_TAG" true
+and findArangoDBVersion
+or begin unlockDirectory ; exit 1 ; end
+
+function upload
+  cd /mnt/buildfiles/stage2
+  set -l HOST live.9c5a08db-bfdf-42bc-9393-00c9fdf4c90f@appserver.live.9c5a08db-bfdf-42bc-9393-00c9fdf4c90f.drush.in
+
+  rsync --backup --backup-dir=.backup --exclude=.backup -exclude=*~ --delete -rvvz -e "ssh -i $HOME/.ssh/pantheon -p 2222" $ARANGODB_PACKAGES/snippets/Community/ $HOST:files/d/download-current/
+  and rsync --backup --backup-dir=.backup --exclude=.backup -exclude=*~ --delete -rvvz -e "ssh -i $HOME/.ssh/pantheon -p 2222" $ARANGODB_PACKAGES/snippets/Enterprise/ $HOST:files/d/download-enterprise/
+end
+
+# there might be internet hickups
+upload
+
+set -l s $status
+unlockDirectory
+exit $s
