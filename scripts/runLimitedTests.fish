1cbe4f24a21df6d1612bc7095f4111759d7a95ea
Frank Celler <frank@arangodb.com>
2019-02-01 12:54:45 +0100

    do not reuse 'd', moved to seperate library

--- scripts/runLimitedTests.fish
+++ scripts/runLimitedTests.fish
@@ -1,4 +1,6 @@
 #!/usr/bin/env fish
+set -g SCRIPTS (dirname (readlink -f (status -f)))
+source $SCRIPTS/lib/tests.fish
 
 set -g repoState ""
 set -g repoStateEnterprise ""
@@ -350,91 +352,6 @@ function log
     echo $l
     echo $l >> $INNERWORKDIR/test.log
   end
-end
-
-function createReport
-  set d (date -u +%F_%H.%M.%SZ)
-  echo $d >> testProtocol.txt
-  echo
-  set -g result GOOD
-  set -l badtests
-  pushd $INNERWORKDIR/tmp
-  for d in *.out
-    set -l localresult GOOD
-    echo Looking at directory $d
-    if test -f "$d/UNITTEST_RESULT_EXECUTIVE_SUMMARY.json"
-      if not grep -q true "$d/UNITTEST_RESULT_EXECUTIVE_SUMMARY.json"
-        set -g result BAD
-        set localresult BAD
-        set f (basename -s out $d)log
-        echo Bad result in $f
-        echo Bad result in $f >> testProtocol.txt
-        set badtests $badtests "Bad result in $f"
-      end
-    end
-    if test -f "$d/UNITTEST_RESULT_CRASHED.json"
-      if not grep -q false "$d/UNITTEST_RESULT_CRASHED.json"
-        set -g result BAD
-        set localresult BAD
-        set f (basename -s out $d)log
-        echo A crash occured in $f
-        echo A crash occured in $f >> testProtocol.txt
-        set badtests $badtests "A crash occured in $f"
-      end
-    end
-    if test -f "$d/started" -a -f "$d/UNITTEST_RESULT_EXECUTIVE_SUMMARY.json"
-      set started (cat "$d/started")
-      set stopped (date -u -r "$d/UNITTEST_RESULT_EXECUTIVE_SUMMARY.json" +%s)
-      echo Test $d took (math $stopped - $started) seconds, status $localresult
-      echo Test $d took (math $stopped - $started) seconds, status $localresult >> testProtocol.txt
-    end
-  end
-
-  if test -e "jslint.log"
-   # this is the jslint output
-    if grep ERROR "jslint.log"
-      set -g result BAD
-      echo Bad result in jslint
-      echo Bad result in jslint >> testProtocol.txt
-      set badtests $badtests "Bad result in jslint"
-      mkdir "$INNERWORKDIR/jslint.out/"
-      grep ERROR "jslint.log" > "$INNERWORKDIR/jslint.out/testfailures.txt"
-    end
-  end
- 
-  popd
-  echo $result >> testProtocol.txt
-  pushd $INNERWORKDIR
-  and begin
-    echo tar czvf "$INNERWORKDIR/ArangoDB/innerlogs.tar.gz" --exclude databases --exclude rocksdb --exclude journals tmp
-    tar czvf "$INNERWORKDIR/ArangoDB/innerlogs.tar.gz" --exclude databases --exclude rocksdb --exclude journals tmp
-    popd
-  end
-  
-  set cores core*
-  set archives *.tar.gz
-  set logs *.log
-  if test (count $cores) -ne 0
-    set binaries build/bin/arangod build/bin/arangodbtests
-    echo tar czvf "$INNERWORKDIR/crashreport-$d.tar.gz" $cores $binaries
-    tar czvf "$INNERWORKDIR/crashreport-$d.tar.gz" $cores $binaries
-  end
-  echo tar czvf "$INNERWORKDIR/testreport-$d.tar.gz" $logs testProtocol.txt $archives
-  tar czvf "$INNERWORKDIR/testreport-$d.tar.gz" $logs testProtocol.txt $archives
-
-  echo rm -rf $cores $archives
-  rm -rf $cores $archives
-
-  # And finally collect the testfailures.txt:
-  rm -rf $INNERWORKDIR/testfailures.txt
-  touch $INNERWORKDIR/testfailures.txt
-  for f in "$INNERWORKDIR"/tmp/*.out/testfailures.txt
-    cat -s $f >> $INNERWORKDIR/testfailures.txt
-  end
-  if grep "unclean shutdown" "$INNERWORKDIR/testfailures.txt"
-    set -g result BAD
-  end
-  log "$d $TESTSUITE $result M:$MAINTAINER $BUILDMODE E:$ENTERPRISEEDITION $STORAGEENGINE" $repoState $repoStateEnterprise $badtests ""
 end
 
 cd $INNERWORKDIR
