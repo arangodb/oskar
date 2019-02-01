c5b1adb145d7074b9de959223a9152184479e31c
Frank Celler <frank@arangodb.com>
2019-02-01 13:45:23 +0100

    cleanout old cores

--- helper.fish
+++ helper.fish
@@ -697,6 +697,13 @@ function clearResults
 end
 
 function cleanWorkspace
+  if test -d /cores
+    set -l cores /cores/core.*
+    if test (count $cores) -ne 0
+      rm -f $cores
+    end
+  end
+
   if test -d $WORKDIR/work
     pushd $WORKDIR/work
     and find . -maxdepth 1 '!' "(" -name ArangoDB -o -name . -o -name .. -o -name ".cc*" ")" -exec rm -rf "{}" ";"
