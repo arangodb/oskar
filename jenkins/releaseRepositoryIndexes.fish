7176fc5087c89633f1680d193b005672312e33f8
Frank Celler <frank@arangodb.com>
2019-02-06 14:01:42 +0100

    added releaseRepositoryIndexes.fish

---
+++ jenkins/releaseRepositoryIndexes.fish
@@ -0,0 +1,16 @@
+#!/usr/bin/env fish
+if test -z "$RELEASE_TAG"
+  echo "RELEASE_TAG required"
+  exit 1
+end
+
+source jenkins/helper.jenkins.fish ; prepareOskar
+
+switchBranches "$RELEASE_TAG" "$RELEASE_TAG" true
+and findArangoDBVersion
+and cd ../file-browser
+and python program.py /mnt/buildfiles/stage2/$ARANGODB_PACKAGES/repositories/Community
+
+set -l s $status
+unlockDirectory
+exit $s
