adcd1d3b9c9ef450ca650d31c08f2706875f4b3d
Frank Celler <frank@arangodb.com>
2019-02-20 22:26:36 +0100

    reformat output

--- jenkins/releaseRepositoryIndexes.fish
+++ jenkins/releaseRepositoryIndexes.fish
@@ -10,9 +10,12 @@ switchBranches "$RELEASE_TAG" "$RELEASE_TAG" true
 and findArangoDBVersion
 and cd $WORKSPACE/file-browser
 and rm -f file-browser.out
-and python program.py /mnt/buildfiles/stage2/$ARANGODB_PACKAGES/repositories/Community 2>&1 | tee file-browser.out
+and python program.py /mnt/buildfiles/stage2/$ARANGODB_PACKAGES/repositories/Community > file-browser.out 2>&1
 
 set -l s $status
+
+echo "File-Browser output:"
+cat file-browser.out
 
 if fgrep -q Errno file-browser.out
   set s 1
