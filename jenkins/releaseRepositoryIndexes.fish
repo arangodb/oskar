8027331656c2675f3ecd12fd4deba6b5582f5e8f
Frank Celler <frank@arangodb.com>
2019-02-21 10:35:01 +0100

    update file structure

--- jenkins/releaseRepositoryIndexes.fish
+++ jenkins/releaseRepositoryIndexes.fish
@@ -10,7 +10,12 @@ switchBranches "$RELEASE_TAG" "$RELEASE_TAG" true
 and findArangoDBVersion
 and cd $WORKSPACE/file-browser
 and rm -f file-browser.out
-and python program.py /mnt/buildfiles/stage2/$ARANGODB_PACKAGES/repositories/Community > file-browser.out 2>&1
+and rm -rf root-dir
+and mkdir root-dir
+and ln -s /mnt/buildfiles/stage2/$ARANGODB_PACKAGES/packages/Community root-dir/$ARANGODB_PACKAGES/Community
+and ln -s /mnt/buildfiles/stage2/$ARANGODB_PACKAGES/repositories/Debian root-dir/$ARANGODB_PACKAGES/Debian
+and ln -s /mnt/buildfiles/stage2/$ARANGODB_PACKAGES/repositories/RPM root-dir/$ARANGODB_PACKAGES/RPM
+and python program.py root-dir > file-browser.out 2>&1
 
 set -l s $status
 
