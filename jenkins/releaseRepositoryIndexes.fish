2dd870ecfd14dbb40414396c348594aba4b3fdaf
Frank Celler <frank@arangodb.com>
2019-02-21 11:14:37 +0100

    fixed paths

--- jenkins/releaseRepositoryIndexes.fish
+++ jenkins/releaseRepositoryIndexes.fish
@@ -13,7 +13,7 @@ and rm -f file-browser.out
 and rm -rf root-dir
 and mkdir -p root-dir/$ARANGODB_REPO
 and ln -s /mnt/buildfiles/stage2/$ARANGODB_PACKAGES/packages/Community root-dir/$ARANGODB_REPO/Community
-and ln -s /mnt/buildfiles/stage2/$ARANGODB_PACKAGES/repositories/Community/Debian root-dir/$ARANGODB_REPO/Debian
+and ln -s /mnt/buildfiles/stage2/$ARANGODB_PACKAGES/repositories/Community/Debian root-dir/$ARANGODB_REPO/DEBIAN
 and ln -s /mnt/buildfiles/stage2/$ARANGODB_PACKAGES/repositories/Community/RPM root-dir/$ARANGODB_REPO/RPM
 and python program.py root-dir > file-browser.out 2>&1
 and cp root-dir/$ARANGODB_REPO/index.html /mnt/buildfiles/stage2/$ARANGODB_PACKAGES/
