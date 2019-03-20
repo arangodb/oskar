fd052cbaecf62df047f2a81546b6e9f0cb66834f
KVS85 <vadim@arangodb.com>
2019-03-20 18:12:31 +0300

    Update runCatchTest.ps1

--- jenkins/runCatchTest.ps1
+++ jenkins/runCatchTest.ps1
@@ -6,7 +6,7 @@ Copy-Item -Force "$env:WORKSPACE\jenkins\prepareOskar.ps1" $pwd
 skipPackagingOn
 staticExecutablesOn
 catchtest
-parallelism 24
+parallelism ([int]$env:NUMBER_OF_PROCESSORS*2)
 
 switchBranches $env:ARANGODB_BRANCH $env:ENTERPRISE_BRANCH
 
