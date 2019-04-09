6d33ec396694642535571c4960ef37cd08a000c1
Frank Celler <frank@arangodb.com>
2019-04-09 18:09:12 +0200

    added runAsanNightlyTest.fish

---
+++ jenkins/runAsanNightlyTest.fish
@@ -0,0 +1,21 @@
+#!/usr/bin/env fish
+source jenkins/helper.jenkins.fish ; prepareOskar
+
+lockDirectory ; updateOskar ; clearResults
+
+eval $EDITION ; eval $STORAGE_ENGINE ; eval $TEST_SUITE ; skipGrey
+
+if test -z "$PARALLELISM_FULL_TEST"
+  set -g PARALLELISM_FULL_TEST 20
+end
+
+switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
+and parallelism "$PARALLELISM_FULL_TEST"
+and compiler "$COMPILER_VERSION"
+and asanOn
+and oskar1Full
+
+set -l s $status
+cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory 
+exit $s
+
