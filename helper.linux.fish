21ae81c39ff092705dce313d9907f6cd5d70d404
KVS85 <vadim@arangodb.com>
2019-04-10 19:42:59 +0300

    Update helper.linux.fish

--- helper.linux.fish
+++ helper.linux.fish
@@ -17,9 +17,6 @@ set -gx LDAPNETWORK ldaptestnet
 
 set -gx SYSTEM_IS_LINUX true
 
-echo "$ARANGO_SIGN_PASSWD" > /tmp/ARANGO_SIGN_PASSWD
-echo $ARANGO_SIGN_PASSWD > /tmp/ARANGO_SIGN_PASSWD2
-
 ## #############################################################################
 ## config
 ## #############################################################################
