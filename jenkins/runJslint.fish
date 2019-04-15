aa05cd3e4b176e738c4c8a51c2fd2e0d327f414e
Frank Celler <frank@arangodb.com>
2019-04-15 15:14:54 +0200

    added jslint

---
+++ jenkins/runJslint.fish
@@ -0,0 +1,14 @@
+#!/usr/bin/env fish
+source jenkins/helper.jenkins.fish ; prepareOskar
+
+lockDirectory ; updateOskar ; clearResults
+
+eval $EDITION ; eval $STORAGE_ENGINE ; eval $TEST_SUITE ; skipGrey
+
+switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
+and rm -f work/test.log
+and jslint | tee work/test.log
+
+set -l s $status
+cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory 
+exit $s
