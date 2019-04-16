0dc855d7a9210a2f340f734fd7d3d5ef46dd0e5e
KVS85 <vadim@arangodb.com>
2019-04-17 00:10:17 +0300

    Update helper.psm1

--- helper.psm1
+++ helper.psm1
@@ -877,7 +877,7 @@ Function storeSymbols
 Function setNightlyRelease
 {
     checkoutIfNeeded
-    (Get-Content $global:WORKDIR\work\ArangoDB\CMakeLists.txt) -replace '"set\(ARANGODB_VERSION_RELEASE_TYPE .*"', 'set(ARANGODB_VERSION_RELEASE_TYPE "nightly"' | Out-File $global:WORKDIR\work\ArangoDB\CMakeLists.txt
+    (Get-Content $INNERWORKDIR\ArangoDB\CMakeLists.txt) -replace '"set\(ARANGODB_VERSION_RELEASE_TYPE .*"', 'set(ARANGODB_VERSION_RELEASE_TYPE "nightly"' | Out-File $INNERWORKDIR\ArangoDB\CMakeLists.txt
 }
 
 Function buildArangoDB
