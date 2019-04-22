e4c4e3ff8530e0e68563abf4cbe8c762acf80f29
Frank Celler <frank@arangodb.com>
2019-04-22 22:39:47 +0200

    jenkins build examples

---
+++ jenkins/buildExamples.fish
@@ -0,0 +1,20 @@
+#!/usr/bin/env fish
+source jenkins/helper.jenkins.fish
+and prepareOskar; and lockDirectory; and updateOskar; and clearResults
+and enterprise; and rocksdb; and asanOff; and maintainerOff
+
+if test $status -ne 0
+    echo "failed to prepare environement"
+    exit 1
+end
+
+showConfig
+
+echo Working on branch $ARANGODB_BRANCH of main repository and
+echo on branch $ENTERPRISE_BRANCH of enterprise repository.
+
+buildExamples
+
+set -l s $status
+cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory 
+exit $s
