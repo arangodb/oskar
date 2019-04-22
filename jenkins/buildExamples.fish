72f542d811ec6b299cf49fa0f96e345dfb389411
Frank Celler <frank@arangodb.com>
2019-04-22 22:42:35 +0200

    switch branch

--- jenkins/buildExamples.fish
+++ jenkins/buildExamples.fish
@@ -13,7 +13,8 @@ showConfig
 echo Working on branch $ARANGODB_BRANCH of main repository and
 echo on branch $ENTERPRISE_BRANCH of enterprise repository.
 
-buildExamples
+switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
+and buildExamples
 
 set -l s $status
 cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory 
