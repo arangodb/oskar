b13dcb208dc446135cd5b65f107fcbc43f89a786
Frank Celler <frank@arangodb.com>
2019-04-22 19:28:56 +0200

    save status

--- jenkins/runJslint.fish
+++ jenkins/runJslint.fish
@@ -7,7 +7,7 @@ eval $EDITION ; eval $STORAGE_ENGINE ; eval $TEST_SUITE ; skipGrey
 
 switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
 and rm -f work/test.log
-and jslint ^| tee work/test.log
+and begin jslint; set -g s $status end ^| tee work/test.log
 
 set -l s $status
 cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory 
