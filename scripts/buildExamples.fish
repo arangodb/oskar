b3a8cd0a2c99524fbddaad23cb82fddaad6569f0
KVS85 <vadim@arangodb.com>
2019-04-23 15:07:26 +0300

    Add allComments.txt separate generation

--- scripts/buildExamples.fish
+++ scripts/buildExamples.fish
@@ -7,8 +7,9 @@ and echo "Generating examples"
 and bash -c ./utils/generateExamples.sh
 and echo "Generating swagger"
 and bash -c ./utils/generateSwagger.sh
+and bash -c "cd Documentation/Scripts && python ./codeBlockReader.py"
 and rm -rf ../Documentation
 and mkdir ../Documentation
-and cp -a Documentation/Examples js/apps/system/_admin/aardvark/APP/api-docs.json ../Documentation
+and cp -a Documentation/Examples js/apps/system/_admin/aardvark/APP/api-docs.json Documentation/Scripts/allComments.txt ../Documentation
 or begin echo "FAILED!"; popd; exit 1; end
 popd
