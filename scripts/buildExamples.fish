b75dfb83a3208b24f6ca87c911497bae0da0b505
KVS85 <vadim@arangodb.com>
2019-04-24 17:51:52 +0300

    Add options dump custom

--- scripts/buildExamples.fish
+++ scripts/buildExamples.fish
@@ -11,5 +11,6 @@ and bash -c "cd Documentation/Scripts && python ./codeBlockReader.py"
 and rm -rf ../Documentation
 and mkdir ../Documentation
 and cp -a Documentation/Examples js/apps/system/_admin/aardvark/APP/api-docs.json Documentation/Scripts/allComments.txt ../Documentation
+and for i in ../Documentation/Examples/arango*.json; mv $i ../Documentation/(basename $i .json)-options.json; end
 or begin echo "FAILED!"; popd; exit 1; end
 popd
