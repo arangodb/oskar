b3e6d4dc1d70e75ca5579b5bb1a3818df1becd9e
KVS85 <vadim@arangodb.com>
2019-05-03 15:52:49 +0300

    Use docs repo for examples (#114)

--- scripts/buildExamples.fish
+++ scripts/buildExamples.fish
@@ -1,5 +1,20 @@
 #!/usr/bin/env fish
 pushd $INNERWORKDIR/ArangoDB
+and if test ! -d docs
+  git clone ssh://git@github.com/arangodb/docs
+end
+and begin
+  set -l CMAKELIST "CMakeLists.txt"
+  set -l AV "set(ARANGODB_VERSION"
+  set -l SEDFIX 's/.*"\([0-9a-zA-Z]*\)".*$/\1/'
+
+  set -xg ARANGODB_VERSION (grep "$AV""_MAJOR" $CMAKELIST | sed -e $SEDFIX)"."(grep "$AV""_MINOR" $CMAKELIST | sed -e $SEDFIX)
+end
+and begin ; rm -rf "Documentation/Books/AQL/*" ; cp docs/$ARANGODB_VERSION/aql/*.md Documentation/Books/AQL/ ; end
+and begin ; rm -rf "Documentation/Books/Manual/*" ; cp docs/$ARANGODB_VERSION/*.md Documentation/Books/Manual/ ; end
+and begin ; rm -rf "Documentation/Books/HTTP/*" ; cp docs/$ARANGODB_VERSION/http/*.md Documentation/Books/HTTP/ ; end
+and begin ; rm -rf "Documentation/Books/Cookbook/*" ; cp docs/$ARANGODB_VERSION/cookbook/*.md Documentation/Books/Cookbook/ ; end
+and begin ; rm -rf "Documentation/Books/Drivers/*" ; cp docs/$ARANGODB_VERSION/drivers/*.md Documentation/Books/Drivers/ ; end
 and rm -rf js/apps/system/_admin/aardvark/APP/api-docs.json
 and rm -rf Documentation/Examples
 and mkdir Documentation/Examples
