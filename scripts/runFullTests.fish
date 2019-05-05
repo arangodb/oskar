c5113c1d16afc6783a0c386051ab69940283c7e6
KVS85 <vadim@arangodb.com>
2019-05-05 22:49:52 +0300

    Refactor to runTests.fish consistency

--- scripts/runFullTests.fish
+++ scripts/runFullTests.fish
@@ -2,131 +2,192 @@
 set -g SCRIPTS (dirname (status -f))
 source $SCRIPTS/lib/tests.fish
 
+################################################################################
+## Single tests: runtime,command
+################################################################################
+
+set -l ST
+set ST "$ST""1000,runSingleTest1 'upgrade_data_3.2.*' -\n"
+set ST "$ST""1000,runSingleTest1 'upgrade_data_3.3.*' -\n"
+set ST "$ST""1000,runSingleTest1 'upgrade_data_3.4.*' -\n"
+set ST "$ST""1000,runSingleTest2 replication_static -\n"
+set ST "$ST""1000,runSingleTest2 replication_ongoing_32 -\n"
+set ST "$ST""1000,runSingleTest2 replication_ongoing_frompresent_32 -\n"
+set ST "$ST""1000,runSingleTest2 replication_ongoing_frompresent -\n"
+set ST "$ST""1000,runSingleTest2 replication_ongoing_global_spec -\n"
+set ST "$ST""1000,runSingleTest2 replication_ongoing_global -\n"
+set ST "$ST""1000,runSingleTest2 replication_ongoing -\n"
+set ST "$ST""1000,runSingleTest2 replication_aql -\n"
+set ST "$ST""1000,runSingleTest2 replication_fuzz -\n"
+set ST "$ST""1000,runSingleTest2 replication_random - \n"
+set ST "$ST""1000,runSingleTest2 replication_sync -\n"
+hasLDAPHOST; and set ST "$ST""2000,runSingleTest1 ldaprole --ldapHost $LDAPHOST\n"
+hasLDAPHOST; and set ST "$ST""2000,runSingleTest1 ldaprolesimple --ldapHost $LDAPHOST\n"
+hasLDAPHOST; and set ST "$ST""2000,runSingleTest1 ldapsearch --ldapHost $LDAPHOST\n"
+hasLDAPHOST; and set ST "$ST""2000,runSingleTest1 ldapsearchsimple --ldapHost $LDAPHOST\n"
+set ST "$ST""2000,runSingleTest1 recovery 0 --testBuckets 4/0\n"
+set ST "$ST""2000,runSingleTest1 recovery 1 --testBuckets 4/1\n"
+set ST "$ST""2000,runSingleTest1 recovery 2 --testBuckets 4/2\n"
+set ST "$ST""2000,runSingleTest1 recovery 3 --testBuckets 4/3\n"
+set ST "$ST""250,runSingleTest1 shell_server_aql 0 --testBuckets 6/0\n"
+set ST "$ST""750,runSingleTest1 shell_server_aql 1 --testBuckets 6/1\n"
+set ST "$ST""500,runSingleTest1 shell_server_aql 2 --testBuckets 6/2\n"
+set ST "$ST""250,runSingleTest1 shell_server_aql 3 --testBuckets 6/3\n"
+set ST "$ST""250,runSingleTest1 shell_server_aql 4 --testBuckets 6/4\n"
+set ST "$ST""250,runSingleTest1 shell_server_aql 5 --testBuckets 6/5\n"
+set ST "$ST""250,runSingleTest1 server_http -\n"
+set ST "$ST""250,runSingleTest1 ssl_server 0 --testBuckets 2/0\n"
+set ST "$ST""250,runSingleTest1 ssl_server 1 --testBuckets 2/1\n"
+set ST "$ST""750,runSingleTest1 shell_client -\n"
+set ST "$ST""500,runSingleTest1 shell_client_aql -\n"
+set ST "$ST""250,runSingleTest1 shell_replication -\n"
+set ST "$ST""1000,runSingleTest1 shell_server -\n"
+set ST "$ST""250,runSingleTest1 BackupAuthNoSysTests -\n"
+set ST "$ST""250,runSingleTest1 BackupAuthSysTests -\n"
+set ST "$ST""250,runSingleTest1 BackupNoAuthNoSysTests -\n"
+set ST "$ST""250,runSingleTest1 BackupNoAuthSysTests -\n"
+set ST "$ST""250,runSingleTest1 active_failover -\n"
+set ST "$ST""250,runSingleTest1 agency -\n"
+set ST "$ST""250,runSingleTest1 arangobench  -\n"
+set ST "$ST""250,runSingleTest1 arangosh -\n"
+set ST "$ST""250,runSingleTest1 audit -\n"
+set ST "$ST""1000,runSingleTest1 authentication -\n"
+set ST "$ST""1000,runSingleTest1 authentication_parameters -\n"
+set ST "$ST""1000,runSingleTest1 authentication_server -\n"
+set ST "$ST""1000,runSingleTest1 catch -\n"
+set ST "$ST""1000,runSingleTest1 config -\n"
+set ST "$ST""1000,runSingleTest1 dfdb -\n"
+set ST "$ST""250,runSingleTest1 dump -\n"
+set ST "$ST""250,runSingleTest1 dump_authentication -\n"
+set ST "$ST""250,runSingleTest1 dump_encrypted -\n"
+set ST "$ST""250,runSingleTest1 dump_maskings -\n"
+set ST "$ST""250,runSingleTest1 dump_multiple -\n"
+set ST "$ST""250,runSingleTest1 endpoints - --skipEndpointsIpv6 true\n"
+set ST "$ST""250,runSingleTest1 export -\n"
+set ST "$ST""500,runSingleTest1 foxx_manager -\n"
+set ST "$ST""500,runSingleTest1 http_replication -\n"
+set ST "$ST""500,runSingleTest1 http_server 0 --testBuckets 2/0-\n"
+set ST "$ST""500,runSingleTest1 http_server 1 --testBuckets 2/1-\n"
+set ST "$ST""500,runSingleTest1 importing -\n"
+set ST "$ST""500,runSingleTest1 load_balancing -\n"
+set ST "$ST""500,runSingleTest1 load_balancing_auth -\n"
+set ST "$ST""500,runSingleTest1 queryCacheAuthorization -\n"
+set ST "$ST""500,runSingleTest1 readOnly -\n"
+set ST "$ST""500,runSingleTest1 upgrade -\n"
+set ST "$ST""250,runSingleTest1 version -\n"
+set ST "$ST""500,runSingleTest1 audit_client -\n"
+set ST "$ST""500,runSingleTest1 audit_server -\n"
+set ST "$ST""500,runSingleTest1 permissions -\n"
+set ST "$ST""500,runSingleTest1 permissions_server -\n"
+
+set -g STS (echo -e $ST | fgrep , | sort -rn | awk -F, '{print $2}')
+set -g STL (count $STS)
+
 function launchSingleTests
-  switch $launchCount
-    case  0 ;                   runSingleTest1 'upgrade_data_3.2.*' ""
-    case  1 ;                   runSingleTest1 'upgrade_data_3.3.*' ""
-    case  2 ;                   runSingleTest1 'upgrade_data_3.4.*' ""
-    case  3 ;                   runSingleTest2 replication_static ""
-    case  4 ;                   runSingleTest1 shell_server ""
-    case  5 ;                   runSingleTest2 replication_ongoing_32 ""
-    case  6 ;                   runSingleTest2 replication_ongoing_frompresent_32 ""
-    case  7 ;                   runSingleTest2 replication_ongoing_frompresent ""
-    case  8 ;                   runSingleTest2 replication_ongoing_global_spec ""
-    case  9 ;                   runSingleTest2 replication_ongoing_global ""
-    case 10 ;                   runSingleTest2 replication_ongoing ""
-    case 11 ;                   runSingleTest2 replication_aql ""
-    case 12 ;                   runSingleTest2 replication_fuzz ""
-    case 13 ;                   runSingleTest2 replication_random ""
-    case 14 ;                   runSingleTest2 replication_sync ""
-    case 15 ; hasLDAPHOST;  and runSingleTest1 ldaprole "" --ldapHost $LDAPHOST
-    case 16 ; hasLDAPHOST;  and runSingleTest1 ldaprolesimple "" --ldapHost $LDAPHOST
-    case 17 ; hasLDAPHOST;  and runSingleTest1 ldapsearch "" --ldapHost $LDAPHOST
-    case 18 ; hasLDAPHOST;  and runSingleTest1 ldapsearchsimple "" --ldapHost $LDAPHOST
-    case 19 ;                   runSingleTest1 recovery 0 --testBuckets 4/0
-    case 20 ;                   runSingleTest1 recovery 1 --testBuckets 4/1
-    case 21 ;                   runSingleTest1 recovery 2 --testBuckets 4/2
-    case 22 ;                   runSingleTest1 recovery 3 --testBuckets 4/3
-    case 23 ;                   runSingleTest1 shell_server_aql 0 --testBuckets 6/0
-    case 24 ;                   runSingleTest1 shell_server_aql 1 --testBuckets 6/1
-    case 25 ;                   runSingleTest1 shell_server_aql 2 --testBuckets 6/2
-    case 26 ;                   runSingleTest1 shell_server_aql 3 --testBuckets 6/3
-    case 27 ;                   runSingleTest1 shell_server_aql 4 --testBuckets 6/4
-    case 28 ;                   runSingleTest1 shell_server_aql 5 --testBuckets 6/5
-    case 29 ;                   runSingleTest1 server_http ""
-    case 30 ;                   runSingleTest1 ssl_server 0 --testBuckets 2/0
-    case 31 ;                   runSingleTest1 ssl_server 1 --testBuckets 2/1
-    case 32 ;                   runSingleTest1 shell_client ""
-    case 33 ;                   runSingleTest1 shell_client_aql ""
-    case 34 ;                   runSingleTest1 shell_replication ""
-    case 35 ;                   runSingleTest1 BackupAuthNoSysTests ""
-    case 36 ;                   runSingleTest1 BackupAuthSysTests ""
-    case 37 ;                   runSingleTest1 BackupNoAuthNoSysTests ""
-    case 38 ;                   runSingleTest1 BackupNoAuthSysTests ""
-    case 39 ;                   runSingleTest1 active_failover ""
-    case 40 ;                   runSingleTest1 agency ""
-    case 41 ;                   runSingleTest1 arangobench  ""
-    case 42 ;                   runSingleTest1 arangosh ""
-    case 43 ;                   runSingleTest1 audit ""
-    case 44 ;                   runSingleTest1 authentication ""
-    case 45 ;                   runSingleTest1 authentication_parameters ""
-    case 46 ;                   runSingleTest1 authentication_server ""
-    case 47 ;                   runSingleTest1 catch ""
-    case 48 ;                   runSingleTest1 config ""
-    case 49 ;                   runSingleTest1 dfdb ""
-    case 50 ;                   runSingleTest1 dump ""
-    case 51 ;                   runSingleTest1 dump_authentication ""
-    case 52 ;                   runSingleTest1 dump_encrypted ""
-    case 53 ;                   runSingleTest1 dump_maskings ""
-    case 54 ;                   runSingleTest1 dump_multiple ""
-    case 55 ;                   runSingleTest1 endpoints "" --skipEndpointsIpv6 true
-    case 56 ;                   runSingleTest1 export ""
-    case 57 ;                   runSingleTest1 foxx_manager ""
-    case 58 ;                   runSingleTest1 http_replication ""
-    case 59 ;                   runSingleTest1 http_server 0 --testBuckets 2/0
-    case 60 ;                   runSingleTest1 http_server 1 --testBuckets 2/1
-    case 61 ;                   runSingleTest1 importing ""
-    case 62 ;                   runSingleTest1 load_balancing ""
-    case 63 ;                   runSingleTest1 load_balancing_auth ""
-    case 64 ;                   runSingleTest1 queryCacheAuthorization ""
-    case 65 ;                   runSingleTest1 readOnly ""
-    case 66 ;                   runSingleTest1 upgrade ""
-    case 67 ;                   runSingleTest1 version ""
-    case 68 ;                   runSingleTest1 audit_client ""
-    case 69 ;                   runSingleTest1 audit_server ""
-    case 70 ;                   runSingleTest1 permissions ""
-    case 71 ;                   runSingleTest1 permissions_server ""
-    case '*' ; return 0
-  end
   set -g launchCount (math $launchCount + 1)
+
+  if test $launchCount -gt $STL
+    return 0
+  end
+
+  set -l test $STS[$launchCount]
+
+  if test -n "$TEST"
+    if echo $test | fgrep -q "$TEST"
+      echo "Running test '$test' (contains '$TEST')"
+    else
+      echo "Skipping test '$test' (does not contain '$TEST')"
+      return 1
+    end
+  end
+
+  eval $test
   return 1
 end
+
+################################################################################
+## Catch tests
+################################################################################
 
 function launchCatchTest
   switch $launchCount
-    case  0 ; runCatchTest1         catch ""
+    case  0 ; runCatchTest1 catch -
     case '*' ; return 0
   end
   set -g launchCount (math $launchCount + 1)
   return 1
 end
 
+################################################################################
+## Cluster tests: runtime,command
+################################################################################
+
+set -l CT
+set CT "$CT""600,runClusterTest1 resilience_move -\n"
+set CT "$CT""750,runClusterTest1 resilience_move_view -\n"
+set CT "$CT""500,runClusterTest1 resilience_repair -\n"
+set CT "$CT""750,runClusterTest1 resilience_failover -\n"
+set CT "$CT""500,runClusterTest1 resilience_failover_failure -\n"
+set CT "$CT""500,runClusterTest1 resilience_failover_view -\n"
+set CT "$CT""500,runClusterTest1 resilience_transactions -\n"
+set CT "$CT""250,runClusterTest1 resilience_sharddist -\n"
+set CT "$CT""500,runClusterTest1 shell_server 0 --testBuckets 5/0\n"
+set CT "$CT""1000,runClusterTest1 shell_server 1 --testBuckets 5/1\n"
+set CT "$CT""500,runClusterTest1 shell_server 2 --testBuckets 5/2\n"
+set CT "$CT""500,runClusterTest1 shell_server 3 --testBuckets 5/3\n"
+set CT "$CT""500,runClusterTest1 shell_server 4 --testBuckets 5/4\n"
+set CT "$CT""500,runClusterTest1 shell_client 0 --testBuckets 5/0\n"
+set CT "$CT""500,runClusterTest1 shell_client 1 --testBuckets 5/1\n"
+set CT "$CT""500,runClusterTest1 shell_client 2 --testBuckets 5/2\n"
+set CT "$CT""500,runClusterTest1 shell_client 3 --testBuckets 5/3\n"
+set CT "$CT""250,runClusterTest1 shell_client 4 --testBuckets 5/4\n"
+set CT "$CT""2000,runClusterTest1 shell_server_aql 0 --testBuckets 16/0\n"
+set CT "$CT""1500,runClusterTest1 shell_server_aql 1 --testBuckets 16/1\n"
+set CT "$CT""1500,runClusterTest1 shell_server_aql 2 --testBuckets 16/2\n"
+set CT "$CT""1500,runClusterTest1 shell_server_aql 3 --testBuckets 16/3\n"
+set CT "$CT""1500,runClusterTest1 shell_server_aql 4 --testBuckets 16/4\n"
+set CT "$CT""2000,runClusterTest1 shell_server_aql 5 --testBuckets 16/5\n"
+set CT "$CT""1500,runClusterTest1 shell_server_aql 6 --testBuckets 16/6\n"
+set CT "$CT""1500,runClusterTest1 shell_server_aql 7 --testBuckets 16/7\n"
+set CT "$CT""1500,runClusterTest1 shell_server_aql 8 --testBuckets 16/8\n"
+set CT "$CT""1500,runClusterTest1 shell_server_aql 9 --testBuckets 16/9\n"
+set CT "$CT""1500,runClusterTest1 shell_server_aql 10 --testBuckets 16/10\n"
+set CT "$CT""1500,runClusterTest1 shell_server_aql 11 --testBuckets 16/11\n"
+set CT "$CT""1500,runClusterTest1 shell_server_aql 12 --testBuckets 16/12\n"
+set CT "$CT""1500,runClusterTest1 shell_server_aql 13 --testBuckets 16/13\n"
+set CT "$CT""1500,runClusterTest1 shell_server_aql 14 --testBuckets 16/14\n"
+set CT "$CT""1500,runClusterTest1 shell_server_aql 15 --testBuckets 16/15\n"
+set CT "$CT""2500,runClusterTest1 shell_client_aql -\n"
+set CT "$CT""500,runClusterTest1 authentication 0 --testBuckets 3/0\n"
+set CT "$CT""500,runClusterTest1 authentication 1 --testBuckets 3/1\n"
+set CT "$CT""500,runClusterTest1 authentication 2 --testBuckets 3/2\n"
+set CT "$CT""750,runClusterTest1 http_server -\n"
+set CT "$CT""1000,runClusterTest1 ssl_server -\n"
+set CT "$CT""500,runClusterTest1 server_http -\n"
+set CT "$CT""250,runClusterTest1 dump -\n"
+set CT "$CT""500,runClusterTest1 client_resilience -\n"
+set CT "$CT""50,runClusterTest1 agency -\n"
+set CT "$CT""50,runClusterTest1 dump_authentication -\n"
+set CT "$CT""250,runClusterTest1 dump_maskings -\n"
+set CT "$CT""250,runClusterTest1 dump_multiple -\n"
+
+set -g CTS (echo -e $CT | fgrep , | sort -rn | awk -F, '{print $2}')
+set -g CTL (count $CTS)
+
 function launchClusterTests
-  switch $launchCount
-    case  0 ; runClusterTest1 resilience_move ""
-    case  1 ; runClusterTest1 resilience_move_view ""
-    case  2 ; runClusterTest1 resilience_repair ""
-    case  3 ; runClusterTest1 resilience_failover ""
-    case  4 ; runClusterTest1 resilience_failover_failure ""
-    case  5 ; runClusterTest1 resilience_failover_view ""
-    case  6 ; runClusterTest1 resilience_transactions ""
-    case  7 ; runClusterTest1 resilience_sharddist ""
-    case  8 ; runClusterTest1 shell_server_aql 3 --testBuckets 6/3
-    case  9 ; runClusterTest1 shell_client ""
-    case 10 ; runClusterTest1 shell_server ""
-    case 11 ; runClusterTest1 shell_server_aql 2 --testBuckets 6/2
-    case 12 ; runClusterTest1 authentication 0 --testBuckets 3/0
-    case 13 ; runClusterTest1 shell_server_aql 0 --testBuckets 6/0
-    case 14 ; runClusterTest1 authentication 2 --testBuckets 3/2
-    case 15 ; runClusterTest1 shell_server_aql 4 --testBuckets 6/4
-    case 16 ; runClusterTest1 shell_server_aql 5 --testBuckets 6/5
-    case 17 ; runClusterTest1 http_server ""
-    case 18 ; runClusterTest1 ssl_server ""
-    case 19 ; runClusterTest1 shell_server_aql 1 --testBuckets 6/1
-    case 20 ; runClusterTest1 authentication 1 --testBuckets 3/1
-    case 21 ; runClusterTest1 shell_client_aql ""
-    case 22 ; runClusterTest1 server_http ""
-    case 23 ; runClusterTest1 dump ""
-    case 24 ; runClusterTest1 client_resilience ""
-    case 25 ; runClusterTest1 agency ""
-    case 26 ; runClusterTest1 dump_authentication ""
-    case 27 ; runClusterTest1 dump_maskings ""
-    case 28 ; runClusterTest1 dump_multiple ""
-    case '*' ; return 0
-  end
   set -g launchCount (math $launchCount + 1)
+
+  if test $launchCount -gt $CTL
+    return 0
+  end
+
+  eval $CTS[$launchCount]
   return 1
 end
+
+################################################################################
+## main
+################################################################################
 
 # Switch off jemalloc background threads for the tests since this seems
 # to overload our systems and is not needed.
@@ -135,22 +196,42 @@ set -x MALLOC_CONF background_thread:false
 setupTmp
 cd $INNERWORKDIR/ArangoDB
 
+if test "$ASAN" = "On"
+  ulimit -c 0
+end
+
 switch $TESTSUITE
   case "cluster"
     resetLaunch 4
-    waitOrKill 10800 launchClusterTests
+    and if test "$ASAN" = "On"
+      waitOrKill 43200 launchClusterTests
+    else
+      waitOrKill 10800 launchClusterTests
+    end
     createReport
   case "single"
     resetLaunch 1
-    waitOrKill 7200 launchSingleTests
+    and if test "$ASAN" = "On"
+      waitOrKill 28800 launchSingleTests
+    else
+      waitOrKill 7200 launchSingleTests
+    end
     createReport
   case "catchtest"
     resetLaunch 1
-    waitOrKill 1800 launchCatchTest
+    and if test "$ASAN" = "On"
+      waitOrKill 14400 launchCatchTest
+    else
+      waitOrKill 1800 launchCatchTest
+    end
     createReport
   case "resilience"
     resetLaunch 4
-    waitOrKill 10800 launchResilienceTests
+    and if test "$ASAN" = "On"
+      waitOrKill 43200 launchResilienceTests
+    else
+      waitOrKill 10800 launchResilienceTests
+    end
     createReport
   case "*"
     echo Unknown test suite $TESTSUITE
