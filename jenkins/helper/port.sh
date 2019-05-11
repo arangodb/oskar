14df09bddb203912b12a7b3c9dac58d8611be1d7
Frank Celler <frank@arangodb.com>
2019-05-11 09:21:57 +0200

    helper scripts

---
+++ jenkins/helper/port.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+TIMEOUT=180 # in minutes
+PORTDIR=/var/tmp/ports
+
+mkdir -p $PORTDIR
+
+if test "$1" == "--clean"; then
+    shift
+
+    while test $# -gt 0; do
+        echo "freeing port $1"
+        rm -f $PORTDIR/$1
+        shift
+    done
+
+    exit
+fi
+
+port=9000
+INCR=1
+
+find $PORTDIR -type f -cmin +$TIMEOUT -exec rm "{}" ";"
+
+while ! ((set -o noclobber ; date > $PORTDIR/$port) 2> /dev/null); do
+    sleep 1
+    port=`expr $port + $INCR`
+done
+
+echo $port
