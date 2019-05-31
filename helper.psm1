ffc8b04517c9e78d4ac4bc7f8ddbfdeb6e0ae192
KVS85 <vadim@arangodb.com>
2019-05-31 15:39:53 +0300

    Disable uploadSymbols due to problems with gsutil

--- helper.psm1
+++ helper.psm1
@@ -1009,7 +1009,7 @@ Function storeSymbols
             Write-Host "Symbol: symstore.exe add /f `"$SYMBOL`" /s `"S:\symsrv_arangodb$global:ARANGODB_VERSION_MAJOR$global:ARANGODB_VERSION_MINOR`" /t ArangoDB /compress"
             proc -process "symstore.exe" -argument "add /f `"$SYMBOL`" /s `"S:\symsrv_arangodb$global:ARANGODB_VERSION_MAJOR$global:ARANGODB_VERSION_MINOR`" /t ArangoDB /compress" -logfile "$INNERWORKDIR\symstore" -priority "Normal"
         }
-        uploadSymbols
+        #uploadSymbols
         Pop-Location
     }
 }
