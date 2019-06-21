ae1b17a795728acb982138b6403ee4cb1c927655
Frank Celler <frank@arangodb.com>
2019-06-21 09:57:17 +0200

    result page

---
+++ jenkins/runSimplePerformanceResults.fish
@@ -0,0 +1,37 @@
+#!/usr/bin/env fish
+
+mkdir -p work/total
+mkdir -p work/images
+
+set -l gp work/generate.gnuplot
+
+echo > $gp
+begin
+  echo 'set yrange [0:]'
+  echo 'set format x "%12.0f"'
+  echo 'set term png size 2048,800'
+end >> $gp
+
+set -l tests (awk -F, '{print $3}' /mnt/buildfiles/performance/results-*.csv | sort | uniq)
+
+for test in $tests
+  echo "set output \"work/images/$test.png\"" >> $gp
+  echo -n 'plot ' >> $gp
+  set -l sep ""
+
+  for v in 3.4 3.5 devel
+    awk -F, "\$1 == \"$v\" && \$3 == \"$test\" {print \$2 \" \" \$5}" results-*.csv > work/total/$v-$test.csv
+
+    echo -n "$sep\"work/total/$v-$test.csv\" with linespoints linewidth 3 title '$v'" >> $gp
+    set -l sep ", "
+  end
+
+  echo >> $gp
+  echo >> $gp
+end
+
+if test (count work/images/*.png) -gt 0
+  rm -f work/images/*.png
+end
+
+gnuplot $gp
