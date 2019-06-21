77f60ed810d96cdc4328314d9ba75132a20770ab
Frank Celler <frank@arangodb.com>
2019-06-21 10:06:32 +0200

    fixes for pathes

--- jenkins/runSimplePerformanceResults.fish
+++ jenkins/runSimplePerformanceResults.fish
@@ -4,26 +4,32 @@ mkdir -p work/total
 mkdir -p work/images
 
 set -l gp work/generate.gnuplot
+set -l d /mnt/buildfiles/performance
 
 echo > $gp
 begin
   echo 'set yrange [0:]'
   echo 'set format x "%12.0f"'
   echo 'set term png size 2048,800'
+  echo
 end >> $gp
 
-set -l tests (awk -F, '{print $3}' /mnt/buildfiles/performance/results-*.csv | sort | uniq)
+set -l tests (awk -F, '{print $3}' $d/results-*.csv | sort | uniq)
 
 for test in $tests
+  echo "Test $test"
+
   echo "set output \"work/images/$test.png\"" >> $gp
   echo -n 'plot ' >> $gp
   set -l sep ""
 
   for v in 3.4 3.5 devel
-    awk -F, "\$1 == \"$v\" && \$3 == \"$test\" {print \$2 \" \" \$5}" results-*.csv > work/total/$v-$test.csv
+    awk -F, "\$1 == \"$v\" && \$3 == \"$test\" {print \$2 \" \" \$5}" $d/results-*.csv > work/total/$v-$test.csv
 
-    echo -n "$sep\"work/total/$v-$test.csv\" with linespoints linewidth 3 title '$v'" >> $gp
-    set -l sep ", "
+    if test -s work/total/$v-$test.csv
+      echo -n "$sep\"work/total/$v-$test.csv\" with linespoints linewidth 3 title '$v'" >> $gp
+      set sep ", "
+    end
   end
 
   echo >> $gp
@@ -34,4 +40,5 @@ if test (count work/images/*.png) -gt 0
   rm -f work/images/*.png
 end
 
+echo "Generating images"
 gnuplot $gp
