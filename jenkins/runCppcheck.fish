a403dab4d7af0851a386462c9675dbdbbe777e09
Frank Celler <frank@arangodb.com>
2019-08-08 12:01:26 +0200

    added build step

--- jenkins/runCppcheck.fish
+++ jenkins/runCppcheck.fish
@@ -4,8 +4,8 @@ source jenkins/helper.jenkins.fish ; prepareOskar
 lockDirectory ; updateOskar ; clearResults
 
 switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
-and rm -f work/test.log
-and jslint > work/test.log
+and buildStaticArangoDB
+and cppcheckArangoDB
 
 set -l s $status
 
