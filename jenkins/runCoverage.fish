e5e992b7cd93a0fb331077a5908341b3428d3f5f
Frank Celler <frank@arangodb.com>
2019-08-13 09:27:08 +0200

    to not stop on failure

--- jenkins/runCoverage.fish
+++ jenkins/runCoverage.fish
@@ -1,58 +1,65 @@
 #!/usr/bin/env fish
 source jenkins/helper/jenkins.fish
 
+set s 0
+
 cleanPrepareLockUpdateClear
 and coverageOn
 and skipGrey
 and switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
 and buildStaticArangoDB -DUSE_FAILURE_TESTS=On -DDEBUG_SYNC_REPLICATION=On
-and oskar1
-
 and begin
-  set -l c 0
+  rm -rf /work/gcov
+  and oskar1
+  or set s $status
 
-  pushd /work
-  and rm -rf combined
-  and mkdir combined
+  begin
+    set -l c 0
 
-  and for i in gcov/????????????????????????????????
-    if test $c -eq 0
-      echo "first file $i"
-      and cp -a $i combined/1
-      and set c 1
-    else if test $c -eq 1
-      echo "merging $i"
-      and rm -rf combined/2
-      and gcov-tool merge $i combined/1 -o combined/2
-      and set c 2
-    else if test $c -eq 2
-      echo "merging $i"
-      and rm -rf combined/1
-      and gcov-tool merge $i combined/2 -o combined/1
-      and set c 1
+    pushd /work
+    and rm -rf combined
+    and mkdir combined
+
+    and for i in gcov/????????????????????????????????
+      if test $c -eq 0
+	echo "first file $i"
+	and cp -a $i combined/1
+	and set c 1
+      else if test $c -eq 1
+	echo "merging $i"
+	and rm -rf combined/2
+	and gcov-tool merge $i combined/1 -o combined/2
+	and set c 2
+      else if test $c -eq 2
+	echo "merging $i"
+	and rm -rf combined/1
+	and gcov-tool merge $i combined/2 -o combined/1
+	and set c 1
+      end
     end
+
+    and if test $c -eq 1
+      mv combined/1 combined/result
+    else if test $c -eq 2
+      mv combined/2 combined/result
+    end
+
+    and rm -rf combined/1 combined/2 /tmp/gcno
+
+    and echo "creating gcno tar"
+    and pushd /work/ArangoDB/build
+    and tar c -f /tmp/gcno.tar (find . -name "*.gcno")
+    and popd
+    and echo "copying gcno files"
+    and tar x -f /tmp/gcno.tar -C /work/combined/result
+
+    and gcovr --root /work/ArangoDB -x -e 3rdParty -o combined/coverage.xml
+
+    and popd
   end
-
-  and if test $c -eq 1
-    mv combined/1 combined/result
-  else if test $c -eq 2
-    mv combined/2 combined/result
-  end
-
-  and rm -rf combined/1 combined/2 /tmp/gcno
-
-  and echo "creating gcno tar"
-  and pushd /work/ArangoDB/build
-  and tar c -f /tmp/gcno.tar (find . -name "*.gcno")
-  and popd
-  and echo "copying gcno files"
-  and tar x -f /tmp/gcno.tar -C /work/combined/result
-
-  and gcovr --root /work/ArangoDB -x -e 3rdParty -o combined/coverage.xml
-
-  and popd
+  or set s $status
 end
+or set s $status
 
-set -l s $status
 cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory 
 exit $s
