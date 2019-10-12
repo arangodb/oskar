cc9d71cee6ac5d1903dea0d8ab242012bc4b3eab
Frank Celler <frank@arangodb.com>
2019-10-12 16:09:50 +0200

    save csv

--- jenkins/runCompilerPerformance.fish
+++ jenkins/runCompilerPerformance.fish
@@ -1,13 +1,33 @@
 #!/usr/bin/env fish
 source jenkins/helper/jenkins.fish
 
+set -xg date (date +%Y%m%d)
+set -xg datetime (date +%Y%m%d%H%M)
+set -xg dest /mnt/buildfiles/performance/Linux/Compiler/RAW
+
 cleanPrepareLockUpdateClear
 and enterprise
 and switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
+and if echo "$ARANGODB_BRANCH" | grep -q "^v"
+  pushd work/ArangoDB
+  set -xg date (git log -1 --format=%aI  | tr -d -- '-:T+' | cut -b 1-8)
+  set -xg datetime (git log -1 --format=%aI  | tr -d -- '-:T+' | cut -b 1-12)
+  echo "==== date $datetime ===="
+  popd
+end
 and pingDetails
 and ccacheOff
+and showConfig
 and buildStaticArangoDB
 
 set -l s $status
+set -l resultname (echo $ARANGODB_BRANCH | tr "/" "_")
+set -l filename $dest/results-$resultname-$datetime.csv
+
+echo "storing results in $resultname"
+awk "{print \"$ARANGODB_BRANCH,$date,\" \$1 \",\" \$2}" \
+  < buildTimes.csv \
+  > $filename
+
 cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory 
 exit $s
