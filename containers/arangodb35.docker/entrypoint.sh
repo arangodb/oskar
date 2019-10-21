81ba2541dc8edda5c453cf190e42b0f9c6d77643
Herve Nicol <herve.nicol@gmail.com>
2019-10-21 10:48:10 +0200

    docker entrypoint - fix typo in a coment

--- containers/arangodb35.docker/entrypoint.sh
+++ containers/arangodb35.docker/entrypoint.sh
@@ -39,7 +39,7 @@ updateconf() {
         # :: so let's split it in 2 separate parsings
         section=$(echo "$var" | sed -n 's/arangoconf_\([^_]*\)_\(.*\)/\1/p')
         param=$(echo "$var" | sed -n 's/arangoconf_\([^_]*\)_\(.*\)/\2/p')
-        param=$(echo "$param" | sed 's/_/-/') # env vars use '-' but param names use '-'
+        param=$(echo "$param" | sed 's/_/-/') # env vars use '_' but param names use '-'
 
         # :: array not available in POSIX sh - let's hope sections are never duplicate
         file="$(ls "$tmpconfdir"/[0-9][0-9]-"$section" 2>/dev/null || true)"
