20981ad3b4ebbcd3404e1152cd7d782c3c1cf672
Frank Celler <frank@arangodb.com>
2019-10-25 15:52:47 +0200

    moved to lib

--- scripts/buildAlpine3.fish
+++ scripts/buildAlpine3.fish
@@ -1,4 +1,6 @@
 #!/usr/bin/env fish
+source ./scripts/lib/build.fish
+
 if test "$PARALLELISM" = ""
   set -xg PARALLELISM 64
 end
@@ -22,46 +24,7 @@ if test "$OPENSSL_VERSION" = ""
 end
 echo "Using openssl version $OPENSSL_VERSION"
 
-if test "$USE_CCACHE" = "Off"
-  set -xg CCACHE_DISABLE true
-  echo "ccache is DISABLED"
-else if test "$USE_CCACHE" = "sccache"
-  if test "$CCACHEBINPATH" = ""
-    set -xg CCACHEBINPATH /tools
-  end
-  if test "$CCACHESIZE" = ""
-    set -xg SCCACHE_CACHE_SIZE 50G
-  else
-    set -xg SCCACHE_CACHE_SIZE $CCACHESIZE
-  end
-  if test "$SCCACHE_REDIS" != ""
-    echo "using sccache at redis ($SCCACHE_REDIS)"
-  else if test "$SCCACHE_GCS_BUCKET" != ""
-    echo "using sccache at GCS ($SCCACHE_GCS_BUCKET)"
-    set -xg SCCACHE_GCS_RW_MODE READ_WRITE
-  else
-   set -xg SCCACHE_DIR $INNERWORKDIR/.sccache.alpine3
-   echo "using sccache at $SCCACHE_DIR ($SCCACHE_CACHE_SIZE)"
-  end
-  pushd $INNERWORKDIR; and sccache --start-server; and popd
-  or begin echo "fatal, cannot start sccache"; exit 1; end
-else
-  cd $INNERWORKDIR
-  mkdir -p .ccache.alpine3
-  set -x CCACHE_DIR $INNERWORKDIR/.ccache.alpine3
-  if test "$CCACHEBINPATH" = ""
-    set -xg CCACHEBINPATH /usr/lib/ccache/bin
-  end
-  if test "$CCACHESIZE" = ""
-    set -xg CCACHESIZE 50G
-  end
-  ccache -M $CCACHESIZE
-  rm -f $INNERWORKDIR/.ccache.log
-  echo "using ccache at $CCACHE_DIR ($CCACHESIZE)"
-  ccache --zero-stats
-  or begin echo "fatal, cannot start ccache"; exit 1; end
-end
-
+setupCache
 cd $INNERWORKDIR/ArangoDB
 
 if test -z "$NO_RM_BUILD"
