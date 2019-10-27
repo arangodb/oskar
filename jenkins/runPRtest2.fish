63572fb7223fe56b60aa974ffcfb0e242678b287
Frank Celler <frank@arangodb.com>
2019-10-27 19:06:39 +0100

    save compile times

--- jenkins/runPRtest2.fish
+++ jenkins/runPRtest2.fish
@@ -11,5 +11,16 @@ and pingDetails
 and oskar1
 
 set -l s $status
+
+if test -f work/buildTimes.csv
+  set -l resultname (echo $ARANGODB_BRANCH | tr "/" "_")
+  set -l filename work/compile-times-$resultname-$datetime.csv
+
+  echo "storing compile times in $resultname"
+  awk -F, "{print \"$ARANGODB_BRANCH,$date,\" \$2 \",\" \$3}" \
+    < work/buildTimes.csv \
+    > $filename
+end
+
 cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory 
 exit $s
