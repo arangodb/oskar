23fa10f3d860c3028a00fc12a1ec390f17c281b3
Frank Celler <frank@arangodb.com>
2019-10-28 10:13:05 +0100

    fixed typos

--- jenkins/runPRtest2.fish
+++ jenkins/runPRtest2.fish
@@ -1,4 +1,5 @@
 #!/usr/bin/env fish
+set -l date (date +%Y%m%d)
 set -l t1 (date +%s)
 set -l filename work/totalTimes.csv
 
@@ -9,14 +10,15 @@ and eval $EDITION
 and eval $STORAGE_ENGINE
 and eval $TEST_SUITE
 and skipGrey
+and rm -f $filename
 and switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
-and pingDetails
 and set -l t2 (date +%s)
-and echo $date,setup,(expr $t2 - $t1) >> $filename
+and echo "$date,setup,"(expr $t2 - $t1) >> $filename
+and pingDetails
 and oskarCompile
 and set -l t3 (date +%s)
 and if test -f work/buildTimes.csv
-  awk -F, "{print \"\" \$2 \",\" \$3}" < work/buildTimes.csv >> $filename
+  awk -F, "{print \"$date\" \$2 \",\" \$3}" < work/buildTimes.csv >> $filename
   and rm -f work/buildTimes.csv
 end
 and oskar
@@ -24,7 +26,7 @@ and oskar
 set -l s $status
 
 set -l t4 (date +%s)
-echo $date,tests,(expr $t4 - $t3) >> $filename
+echo "$date,tests,"(expr $t4 - $t3) >> $filename
 
 cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory 
 exit $s
