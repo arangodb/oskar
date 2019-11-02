bde4296094117f50ef79f57891ab0c44f9128929
Frank Celler <frank@arangodb.com>
2019-11-02 22:31:40 +0100

    cleanup

--- jenkins/statisticsBuildTimes.fish
+++ jenkins/statisticsBuildTimes.fish
@@ -4,48 +4,42 @@ if test -z "$CLOUD_URL"
   exit 1
 end
 
-
-set -l output result.json
-
-echo "copying statistics for job $USE_BUILD_URL"
-curl --user "$USER:$PASSWORD" --insecure -o build.json "$USE_BUILD_URL//api/json?pretty=true&depth=100"
-
-echo '{' > $output
-set -l sep ''
-
-if test -f build.json
-  begin
-    echo '"job": '
-    cat build.json
-    echo $sep
-    set sep ','
-  end >> $output
-end
-
 if test -f totalTimes.csv
   awk -F, '{print($1 ",\"" $2 "\"," $3)}' totalTimes.csv \
     | sed -e 's~\(.*\)~[\1]~' \
     | sed -e 'N;s~\n~,~' \
     | sed -e 's~\(.*\)~[\1]~' \
     > totalTimes.json
+end
 
-  begin
-    echo '"times": '
-    cat totalTimes.json
+echo "copying statistics for job $USE_BUILD_URL"
+curl --user "$USER:$PASSWORD" --insecure -o build.json "$USE_BUILD_URL//api/json?pretty=true&depth=100"
+
+set -l output result.json
+
+echo '{' > $output
+set -l sep ''
+
+for name in build totalTimes
+  if test -f $$name.json
+    begin
+      echo $sep
+      echo \""$name\": "
+      cat $$name.json
+      set sep ','
+    end >> $output
+  end
+end
+
+for name in USE_BUILD_URL USE_JOB_NAME USE_NODE_NAME EDITION STORAGE_ENGINE TEST_SUITE
+  if test ! -z "$$name"
     echo $sep
-    set sep ','
+    echo -n "\"$name\": \"$$name\""
+    set set ','
   end >> $output
 end
 
-begin
-  echo -n '"node": "'
-  if test -z "$NODE_NAME"
-    echo -n unkown
-  else 
-    echo -n $NODE_NAME
-  end
-  echo '" }'
-end >> $output
+echo '}' >> $output
 
 echo "to collection 'times'"
 curl --user "$USER:$PASSWORD" --insecure --header 'accept: application/json' --data-binary "@$output" --dump - -X POST "$CLOUD_URL/_api/document/times"
