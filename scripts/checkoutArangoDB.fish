7025245e28f49095a74f64a4b38a76e60e90c997
Frank Celler <frank@arangodb.com>
2019-11-02 14:53:50 +0100

    fixed typos

--- scripts/checkoutArangoDB.fish
+++ scripts/checkoutArangoDB.fish
@@ -2,7 +2,7 @@
 cd $INNERWORKDIR
 and if test ! -d ArangoDB
   git clone ssh://git@github.com/arangodb/ArangoDB
-else if test -d ArangoDB/.git
+else if test ! -d ArangoDB/.git
   rm -rf ArangoDB
   git clone ssh://git@github.com/arangodb/ArangoDB
 end
