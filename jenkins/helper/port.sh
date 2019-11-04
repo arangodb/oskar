d1d7cbbffffa114a848313550690f49d1444b6c3
KVS85 <vadim@arangodb.com>
2019-11-04 19:58:30 +0300

    Update port.sh

--- jenkins/helper/port.sh
+++ jenkins/helper/port.sh
@@ -4,46 +4,57 @@ PORTDIR=/var/tmp/ports
 
 mkdir -p $PORTDIR
 
-if test "$1" == "--clean"; then
+if test "$1" == "--cluster" ; then
     shift
-
-    while test $# -gt 0; do
-        echo "freeing port $1"
-        rm -f $PORTDIR/$1
+    if test "$2" == "--clean"; then
         shift
-    done
 
-    exit
+        while test $# -gt 0; do
+            echo "freeing port $1"
+            rm -f $PORTDIR/$1
+            shift
+        done
+
+        exit
+    fi
+else
 fi
 
-port=9001
+port=9000
 INCR=1
 
 find $PORTDIR -type f -cmin +$TIMEOUT -exec rm "{}" ";"
 
 if test "$1" == "--cluster" ; then
-  while ! ((set -o noclobber ; date > $PORTDIR/`expr $port - 1` && \
-                               date > $PORTDIR/$port && \
+  shift
+  while ! ((set -o noclobber ; date > $PORTDIR/$port && \
                                date > $PORTDIR/`expr $port + 1` && \
                                date > $PORTDIR/`expr $port + 2` && \
-                               date > $PORTDIR/`expr $port + 9` && \
+                               date > $PORTDIR/`expr $port + 3` && \
                                date > $PORTDIR/`expr $port + 10` && \
                                date > $PORTDIR/`expr $port + 11` && \
                                date > $PORTDIR/`expr $port + 12` && \
-                               date > $PORTDIR/`expr $port + 19` && \
+                               date > $PORTDIR/`expr $port + 13` && \
                                date > $PORTDIR/`expr $port + 20` && \
                                date > $PORTDIR/`expr $port + 21` && \
-                               date > $PORTDIR/`expr $port + 22`) 2> /dev/null); do
+                               date > $PORTDIR/`expr $port + 22` && \
+                               date > $PORTDIR/`expr $port + 23`) 2> /dev/null); do
     sleep 1
     port=`expr $port + $INCR`
   done
+  
+  echo "`expr $port + 1` `expr $port + 11` `expr $port + 21`" > ports
 
-  echo "$port `expr $port + 10` `expr $port + 20`"
+  echo "$port `expr $port + 1` $port `expr $port + 2` `expr $port + 3`\
+        `expr $port + 10` $port `expr $port + 11` `expr $port + 12` `expr $port + 13`\
+        `expr $port + 20` $port `expr $port + 21` `expr $port + 22` `expr $port + 23`\"
 else
-  while ! ((set -o noclobber ; date > $PORTDIR/`expr $port - 1` && date > $PORTDIR/$port) 2> /dev/null); do
+  while ! ((set -o noclobber ; date > $PORTDIR/$port && date > $PORTDIR/`expr $port + 1`) 2> /dev/null); do
     sleep 1
     port=`expr $port + $INCR`
   done
 
-  echo $port
+  echo "`expr $port + 1`" > ports
+  
+  echo "$port `expr $port + 1`"
 fi
