bc89a67ab5ae907bd4bfbe43c5e947d8b9c9de91
KVS85 <vadim@arangodb.com>
2019-11-04 18:32:26 +0300

    Update port.sh

--- jenkins/helper/port.sh
+++ jenkins/helper/port.sh
@@ -16,13 +16,24 @@ if test "$1" == "--clean"; then
     exit
 fi
 
-port=9000
+port=9001
 INCR=1
 
 find $PORTDIR -type f -cmin +$TIMEOUT -exec rm "{}" ";"
 
 if test "$1" == "--cluster" ; then
-  while ! ((set -o noclobber ; date > $PORTDIR/$port && date > $PORTDIR/`expr $port + 10` && date > $PORTDIR/`expr $port + 20`) 2> /dev/null); do
+  while ! ((set -o noclobber ; date > $PORTDIR/`expr $port - 1` && \
+                               date > $PORTDIR/$port && \
+                               date > $PORTDIR/`expr $port + 1` && \
+                               date > $PORTDIR/`expr $port + 2` && \
+                               date > $PORTDIR/`expr $port + 9` && \
+                               date > $PORTDIR/`expr $port + 10` && \
+                               date > $PORTDIR/`expr $port + 11` && \
+                               date > $PORTDIR/`expr $port + 12` && \
+                               date > $PORTDIR/`expr $port + 19` && \
+                               date > $PORTDIR/`expr $port + 20` && \
+                               date > $PORTDIR/`expr $port + 21` && \
+                               date > $PORTDIR/`expr $port + 22`) 2> /dev/null); do
     sleep 1
     port=`expr $port + $INCR`
   done
