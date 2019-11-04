4eb3ce6de4822cc083b4f3731616064fa0c6ab4e
Vadim Kondratyev <vadim@arangodb.com>
2019-11-05 02:59:18 +0300

    Fixed typos

--- jenkins/helper/port.sh
+++ jenkins/helper/port.sh
@@ -20,27 +20,27 @@ port=9000
 INCR=1
 
 find $PORTDIR -type f -cmin +$TIMEOUT -exec rm "{}" ";"
-portfiles=()
 
+echo -n "" > portfiles
 if test "$1" == "--cluster" ; then
   shift
-  while ! ((set -o noclobber ; date > $PORTDIR/$port && portfiles+=("$PORTDIR/$port") &&\
-                               date > $PORTDIR/`expr $port + 1` && portfiles+=("$PORTDIR/`expr $port + 1`") &&\
-                               date > $PORTDIR/`expr $port + 2` && portfiles+=("$PORTDIR/`expr $port + 2`") &&\
-                               date > $PORTDIR/`expr $port + 3` && portfiles+=("$PORTDIR/`expr $port + 3`") &&\
-                               date > $PORTDIR/`expr $port + 10` && portfiles+=("$PORTDIR/`expr $port + 10`") &&\
-                               date > $PORTDIR/`expr $port + 11` && portfiles+=("$PORTDIR/`expr $port + 11`") &&\
-                               date > $PORTDIR/`expr $port + 12` && portfiles+=("$PORTDIR/`expr $port + 12`") &&\
-                               date > $PORTDIR/`expr $port + 13` && portfiles+=("$PORTDIR/`expr $port + 13`") &&\
-                               date > $PORTDIR/`expr $port + 20` && portfiles+=("$PORTDIR/`expr $port + 20`") &&\
-                               date > $PORTDIR/`expr $port + 21` && portfiles+=("$PORTDIR/`expr $port + 21`") &&\
-                               date > $PORTDIR/`expr $port + 22` && portfiles+=("$PORTDIR/`expr $port + 22`") &&\
-                               date > $PORTDIR/`expr $port + 23` && portfiles+=("$PORTDIR/`expr $port + 23`")) 2> /dev/null); do
-    echo ${portfiles[@]}
-    rm -f ${portfiles[@]}
-    sleep 1
+  while ! ((set -o noclobber ; date > $PORTDIR/$port && $(echo -n "$PORTDIR/$port" >> portfiles) &&\
+                               date > $PORTDIR/`expr $port + 1` && $(echo -n " $PORTDIR/`expr $port + 1`" >> portfiles) &&\
+                               date > $PORTDIR/`expr $port + 2` && $(echo -n " $PORTDIR/`expr $port + 2`" >> portfiles) &&\
+                               date > $PORTDIR/`expr $port + 3` && $(echo -n " $PORTDIR/`expr $port + 3`" >> portfiles) &&\
+                               date > $PORTDIR/`expr $port + 10` && $(echo -n " $PORTDIR/`expr $port + 10`" >> portfiles) &&\
+                               date > $PORTDIR/`expr $port + 11` && $(echo -n " $PORTDIR/`expr $port + 11`" >> portfiles) &&\
+                               date > $PORTDIR/`expr $port + 12` && $(echo -n " $PORTDIR/`expr $port + 12`" >> portfiles) &&\
+                               date > $PORTDIR/`expr $port + 13` && $(echo -n " $PORTDIR/`expr $port + 13`" >> portfiles) &&\
+                               date > $PORTDIR/`expr $port + 20` && $(echo -n " $PORTDIR/`expr $port + 20`" >> portfiles) &&\
+                               date > $PORTDIR/`expr $port + 21` && $(echo -n " $PORTDIR/`expr $port + 21`" >> portfiles) &&\
+                               date > $PORTDIR/`expr $port + 22` && $(echo -n " $PORTDIR/`expr $port + 22`" >> portfiles) &&\
+                               date > $PORTDIR/`expr $port + 23` && $(echo -n " $PORTDIR/`expr $port + 23`" >> portfiles)) 2> /dev/null)
+  do
+    sleep 0.5
     port=`expr $port + $INCR`
-    portfiles=()
+    rm -f $(cat portfiles)
+    echo -n "" > portfiles
   done
 
   echo "`expr $port + 1` `expr $port + 11` `expr $port + 21`" > ports
@@ -49,13 +49,13 @@ if test "$1" == "--cluster" ; then
         `expr $port + 10` `expr $port + 11` `expr $port + 12` `expr $port + 13`\
         `expr $port + 20` `expr $port + 21` `expr $port + 22` `expr $port + 23`"
 else
-  while ! ((set -o noclobber ; date > $PORTDIR/$port && portfiles+=("$PORTDIR/$port") &&\
-                               date > $PORTDIR/`expr $port + 1` && portfiles+=("$PORTDIR/`expr $port + 1`")) 2> /dev/null); do
-    echo ${portfiles[@]}
-    rm -f ${portfiles[@]}
-    sleep 1
+  while ! ((set -o noclobber ; date > $PORTDIR/$port && $(echo -n "$PORTDIR/$port") >> portfiles &&\
+                               date > $PORTDIR/`expr $port + 1` && $(echo -n " $PORTDIR/`expr $port + 1`") >> portfiles) 2> /dev/null)
+  do
+    sleep 0.5
     port=`expr $port + $INCR`
-    portfiles=()
+    rm -f $(cat portfiles)
+    echo -n "" > portfiles
   done
 
   echo "`expr $port + 1`" > ports
