cb52b64ae9eee76cf5898d7085eb6368d2f2e79a
Vadim Kondratyev <vadim@arangodb.com>
2019-11-05 00:09:55 +0300

    Properly remove portfiles

--- jenkins/helper/port.sh
+++ jenkins/helper/port.sh
@@ -23,18 +23,20 @@ find $PORTDIR -type f -cmin +$TIMEOUT -exec rm "{}" ";"
 
 if test "$1" == "--cluster" ; then
   shift
-  while ! ((set -o noclobber ; date > $PORTDIR/$port && \
-                               date > $PORTDIR/`expr $port + 1` && \
-                               date > $PORTDIR/`expr $port + 2` && \
-                               date > $PORTDIR/`expr $port + 3` && \
-                               date > $PORTDIR/`expr $port + 10` && \
-                               date > $PORTDIR/`expr $port + 11` && \
-                               date > $PORTDIR/`expr $port + 12` && \
-                               date > $PORTDIR/`expr $port + 13` && \
-                               date > $PORTDIR/`expr $port + 20` && \
-                               date > $PORTDIR/`expr $port + 21` && \
-                               date > $PORTDIR/`expr $port + 22` && \
-                               date > $PORTDIR/`expr $port + 23`) 2> /dev/null); do
+  portfiles = ()
+  while ! ((set -o noclobber ; date > $PORTDIR/$port && portfiles=+("$PORTDIR/$port") &&\
+                               date > $PORTDIR/`expr $port + 1` && portfiles=+("$PORTDIR/`expr $port + 1`") && \
+                               date > $PORTDIR/`expr $port + 2` && portfiles=+("$PORTDIR/`expr $port + 2`") &&\
+                               date > $PORTDIR/`expr $port + 3` && portfiles=+("$PORTDIR/`expr $port + 3`") &&\
+                               date > $PORTDIR/`expr $port + 10` && portfiles=+("$PORTDIR/`expr $port + 10`") &&\
+                               date > $PORTDIR/`expr $port + 11` && portfiles=+("$PORTDIR/`expr $port + 11`") &&\
+                               date > $PORTDIR/`expr $port + 12` && portfiles=+("$PORTDIR/`expr $port + 12`") &&\
+                               date > $PORTDIR/`expr $port + 13` && portfiles=+("$PORTDIR/`expr $port + 13`") &&\
+                               date > $PORTDIR/`expr $port + 20` && portfiles=+("$PORTDIR/`expr $port + 20`") &&\
+                               date > $PORTDIR/`expr $port + 21` && portfiles=+("$PORTDIR/`expr $port + 21`") &&\
+                               date > $PORTDIR/`expr $port + 22` && portfiles=+("$PORTDIR/`expr $port + 22`") &&\
+                               date > $PORTDIR/`expr $port + 23` && portfiles=+("$PORTDIR/`expr $port + 23`")) 2> /dev/null); do
+    rm -f ${portfiles[@]}
     sleep 1
     port=`expr $port + $INCR`
   done
@@ -45,7 +47,10 @@ if test "$1" == "--cluster" ; then
         `expr $port + 10` `expr $port + 11` `expr $port + 12` `expr $port + 13`\
         `expr $port + 20` `expr $port + 21` `expr $port + 22` `expr $port + 23`"
 else
-  while ! ((set -o noclobber ; date > $PORTDIR/$port && date > $PORTDIR/`expr $port + 1`) 2> /dev/null); do
+  portfiles = ()
+  while ! ((set -o noclobber ; date > $PORTDIR/$port && portfiles=+("$PORTDIR/$port") &&\
+                               date > $PORTDIR/`expr $port + 1` && portfiles=+("$PORTDIR/`expr $port + 1`")) 2> /dev/null); do
+    rm -f ${portfiles[@]}
     sleep 1
     port=`expr $port + $INCR`
   done
