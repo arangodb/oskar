cd8002206bbc5adce75e0fea44dc161c1fd701d7
KVS85 <vadim@arangodb.com>
2019-11-20 16:42:41 +0100

    Add Jenkins script to build arbitraty test image

---
+++ jenkins/forTestDocker.fish
@@ -0,0 +1,46 @@
+#!/usr/bin/env fish
+source jenkins/helper/jenkins.fish
+
+if test -z "$DOCKER_TAG"
+  echo "DOCKER_TAG required"
+  exit 1
+end
+
+set -xg TAG "$DOCKER_TAG"
+
+set -xg HUB_COMMUNITY "arangodb/arangodb-test:$TAG"
+set -xg HUB_ENTERPRISE "arangodb/enterprise-test:$TAG"
+
+cleanPrepareLockUpdateClear
+and rm -rf $WORKSPACE/imagenames.log
+and community
+and switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
+and setNightlyRelease
+and findArangoDBVersion
+and buildStaticArangoDB -DTARGET_ARCHITECTURE=nehalem
+and downloadStarter
+and buildDockerImage $HUB_COMMUNITY
+and docker push $HUB_COMMUNITY
+and echo $HUB_COMMUNITY >> $WORKSPACE/imagenames.log
+
+if test $status -ne 0
+  echo Production of community image failed, giving up...
+  cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory
+  exit 1
+end
+
+enterprise
+and switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
+and setNightlyRelease
+and findArangoDBVersion
+and buildStaticArangoDB -DTARGET_ARCHITECTURE=nehalem
+and downloadStarter
+and downloadSyncer
+and buildDockerImage $HUB_ENTERPRISE
+and docker push $HUB_ENTERPRISE
+and echo $HUB_ENTERPRISE >> $WORKSPACE/imagenames.log
+
+set -l s $status
+cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory
+exit $s
+
