d81a6d40d3275488e583723af619eed985d8aac5
KVS85 <vadim@arangodb.com>
2020-03-25 23:29:27 +0300

    Bug fix/use hostname in driver docker (#211)
    * Update server.inc
    
    * Update port.sh
    
    * Update port.sh
    
    * Update server.inc
    
    * Update port.sh
    
    * Update port.sh
    
    * Fixed typos
    
    * Fixed typos
    
    * Fixed typos
    
    * Fixed typos
    
    * Fixed typos
    
    * Fixed typos
    
    * Fixed typos
    
    * Properly remove portfiles
    
    * Fixed typos
    
    * Fixed typos
    
    * Debug output
    
    * Fixed typos
    
    * Debug output
    
    * Fixed typos
    
    * Fixed typos
    
    * Fixed typos
    
    * Try to use flock
    
    * Fix typo
    
    * Fix typo
    
    * Don't use descriptor
    
    * Use another form
    
    * Fix typo
    
    * Backward compatibility
    
    * Add hostname export
    
    * Try localhost explicitly
    
    * Fix ports
    
    * Fix ports 2
    
    * Fix ports 3
    
    * Fix typo
    
    * Remove unnecessary code
    
    * Use less ports and explicit Starter port
    
    * Fix typo
    
    * Fix typo
    
    * Fix typo
    
    * Fix typo
    
    * Add proper cleanup
    
    * Fix typo
    
    * Fix typo
    
    * Fix typo

--- jenkins/helper/port.sh
+++ jenkins/helper/port.sh
@@ -5,16 +5,19 @@ PORTDIR=/var/tmp/ports
 mkdir -p $PORTDIR
 
 if test "$1" == "--clean"; then
+  shift
+
+  while test $# -gt 0; do
+    echo "freeing port $1"
+    rm -f $PORTDIR/$1
     shift
+  done
 
-    while test $# -gt 0; do
-        echo "freeing port $1"
-        rm -f $PORTDIR/$1
-        shift
-    done
-
-    exit
+  exit
 fi
+
+exec 200>/var/lock/$(basename $0)
+flock -w 360 200
 
 port=9000
 INCR=1
@@ -22,17 +25,38 @@ INCR=1
 find $PORTDIR -type f -cmin +$TIMEOUT -exec rm "{}" ";"
 
 if test "$1" == "--cluster" ; then
-  while ! ((set -o noclobber ; date > $PORTDIR/$port && date > $PORTDIR/`expr $port + 10` && date > $PORTDIR/`expr $port + 20`) 2> /dev/null); do
-    sleep 1
+  shift
+  while ! ((set -o noclobber ; date > $PORTDIR/$port && echo "$PORTDIR/$port" > ./ports &&\
+                               date > $PORTDIR/`expr $port + 1` && echo "$PORTDIR/`expr $port + 1`" >> ./ports &&\
+                               date > $PORTDIR/`expr $port + 2` && echo "$PORTDIR/`expr $port + 2`" >> ./ports &&\
+                               date > $PORTDIR/`expr $port + 3` && echo "$PORTDIR/`expr $port + 3`" >> ./ports &&\
+                               date > $PORTDIR/`expr $port + 10` && echo "$PORTDIR/`expr $port + 10`" >> ./ports &&\
+                               date > $PORTDIR/`expr $port + 11` && echo "$PORTDIR/`expr $port + 11`" >> ./ports &&\
+                               date > $PORTDIR/`expr $port + 12` && echo "$PORTDIR/`expr $port + 12`" >> ./ports &&\
+                               date > $PORTDIR/`expr $port + 13` && echo "$PORTDIR/`expr $port + 13`" >> ./ports &&\
+                               date > $PORTDIR/`expr $port + 20` && echo "$PORTDIR/`expr $port + 20`" >> ./ports &&\
+                               date > $PORTDIR/`expr $port + 21` && echo "$PORTDIR/`expr $port + 21`" >> ./ports &&\
+                               date > $PORTDIR/`expr $port + 22` && echo "$PORTDIR/`expr $port + 22`" >> ./ports &&\
+                               date > $PORTDIR/`expr $port + 23` && echo "$PORTDIR/`expr $port + 23`" >> ./ports) 2> /dev/null)
+  do
+    [ -e "./ports" ] && while read -r line; do rm -f "$line"; done < ./ports
+    rm -f ./ports
     port=`expr $port + $INCR`
   done
 
-  echo "$port `expr $port + 10` `expr $port + 20`"
+  echo -n "$port `expr $port + 1` `expr $port + 2` `expr $port + 3`\
+        `expr $port + 10` `expr $port + 11` `expr $port + 12` `expr $port + 13`\
+        `expr $port + 20` `expr $port + 21` `expr $port + 22` `expr $port + 23`"
 else
-  while ! ((set -o noclobber ; date > $PORTDIR/$port) 2> /dev/null); do
-    sleep 1
+  while ! ((set -o noclobber ; date > $PORTDIR/$port && echo "$PORTDIR/$port" > ./ports &&\
+                               date > $PORTDIR/`expr $port + 1` && echo "$PORTDIR/`expr $port + 1`" >> ./ports) 2> /dev/null)
+  do
+    [ -e "./ports" ] && while read -r line; do rm -f "$line"; done < ./ports
+    rm -f ./ports
     port=`expr $port + $INCR`
   done
 
-  echo $port
+  echo -n "$port `expr $port + 1`"
 fi
+
+rm -f ./ports
