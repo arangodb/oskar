15422a382027a3029162f0592604496d4f1a8bf8
KVS85 <vadim@arangodb.com>
2020-04-08 15:35:53 +0200

    Fix old OpenSSL versions URLs

--- containers/buildAlpine2.docker/tools/install-openssl.sh
+++ containers/buildAlpine2.docker/tools/install-openssl.sh
@@ -2,12 +2,23 @@
 set -e
 
 # Compile openssl library:
-export OPENSSLVERSION=$1
+export OPENSSLBRANCH=$1
+export OPENSSLREVISION=$2
+export OPENSSLVERSION=${OPENSSLBRANCH}${OPENSSLREVISION}
+
+echo $OPENSSLBRANCH
+
+if [ "$OPENSSLBRANCH" != "1.1.1" ]; then
+  OLD="old/${OPENSSLBRANCH}/"
+fi;
+
+echo "https://www.openssl.org/source/${OLD}openssl-$OPENSSLVERSION.tar.gz"
+
 test -n "$OPENSSLVERSION"
 export OPENSSLPATH=`echo $OPENSSLVERSION | tr -d "a-zA-Z"`
 cd /tmp
-curl -O https://www.openssl.org/source/openssl-$OPENSSLVERSION.tar.gz
-tar xzf openssl-$OPENSSLVERSION.tar.gz
+curl -O https://www.openssl.org/source/${OLD}openssl-$OPENSSLVERSION.tar.gz
+tar xzvf openssl-$OPENSSLVERSION.tar.gz
 cd openssl-$OPENSSLVERSION
 ./config --prefix=/opt/openssl-$OPENSSLPATH no-async no-shared
 make build_libs
