1504d333a70a6b56da2f6396518779216bdf3a9e
Frank Celler <frank@arangodb.com>
2020-07-02 13:59:35 +0200

    added format type

--- jenkins/helper/performance2json.py
+++ jenkins/helper/performance2json.py
@@ -14,10 +14,15 @@ parser.add_option("-n", "--name", dest="name", help="branch or tag", metavar="BR
 parser.add_option("-m", "--mode", dest="mode", help="singleserver or cluster", metavar="MODE")
 parser.add_option("-e", "--edition", dest="edition", help="community or enterprise", metavar="EDITION")
 parser.add_option("-s", "--size", dest="size", help="tiny, small, medium, big", metavar="SIZE")
+parser.add_option("-F", "--type", dest="input_type", help="input format", metavar="TYPE")
 
 (options, args) = parser.parse_args()
 
-current_date = dt_parser.parse(options.date)
+current_date = None
+
+if options.date:
+        current_date = dt_parser.parse(options.date)
+
 branch = options.branch
 name = options.name
 mode = options.mode
@@ -25,6 +30,7 @@ edition = options.edition
 default_size = options.size
 version_filename = options.version_filename
 version = None
+input_type = options.input_type
 
 if version_filename:
         with open(version_filename) as jsonfile:
@@ -46,36 +52,53 @@ if not version:
         else:
                 version = branch
 
+def simple_performance(row):
+        global version, current_date, default_size
+
+        size = row[11]
+
+        if not size:
+                size = default_size
+
+        if not version:
+                version = row[0]
+
+        date = current_date
+
+        if not date:
+                date = dt_parser.parse(row[1])
+
+        print(json.dumps({
+                "test": {
+                        "name": row[2],
+                        "average": float(row[3]),
+                        "median": float(row[4]),
+                        "min": float(row[5]),
+                        "max": float(row[6]),
+                        "deviation": float(row[7]),
+                        "numberRuns": int(row[10])
+                },
+                "size": {
+                        "collection": row[8],
+                        "count": int(row[9]),
+                        "size": size
+                },
+                "configuration": {
+                        "version": version,
+                        "branch": branch,
+                        "mode": mode,
+                        "edition": edition
+                },
+                "isoDate": date.isoformat(),
+                "date": date.timestamp(),
+                "ms": 1000 * date.timestamp()
+        }))
+
+
 with open(options.filename) as csvfile:
         lines = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in lines:
-                size = row[9]
-
-                if not size:
-                        size = default_size
-
-                print(json.dumps({
-                        "test": {
-                                "name": row[0],
-                                "average": float(row[1]),
-                                "median": float(row[2]),
-                                "min": float(row[3]),
-                                "max": float(row[4]),
-                                "deviation": float(row[5]),
-                                "numberRuns": int(row[8])
-                        },
-                        "size": {
-                                "collection": row[6],
-                                "count": int(row[7]),
-                                "size": size
-                        },
-                        "configuration": {
-                                "version": version,
-                                "branch": branch,
-                                "mode": mode,
-                                "edition": edition
-                        },
-                        "isoDate": current_date.isoformat(),
-                        "date": current_date.timestamp(),
-                        "ms": 1000 * current_date.timestamp()
-                }))
+                if input_type == "simple-performance":
+                        simple_performance(row)
+                else:
+                        print("unknown output format '%s'" % (input_type))
