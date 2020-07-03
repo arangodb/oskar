e4a970b0e30992a7c2394098c712f62fa66faed4
Frank Celler <frank@arangodb.com>
2020-07-03 14:27:48 +0200

    added cluster format

--- jenkins/helper/performance2json.py
+++ jenkins/helper/performance2json.py
@@ -22,6 +22,8 @@ current_date = None
 
 if options.date:
         current_date = dt_parser.parse(options.date)
+else:
+        current_date = dt.datetime.now()
 
 branch = options.branch
 name = options.name
@@ -53,20 +55,47 @@ if not version:
                 version = branch
 
 def simple_performance(row):
-        global version, current_date, default_size
+        global version, current_date, default_size, branch, mode, edition
 
-        size = row[11]
+        size = "big"
+
+        if len(row) > 11:
+                size = row[11]
 
         if not size:
                 size = default_size
 
+        count = 1000000
+
+        if len(row) > 9:
+                count = int(row[9])
+
         if not version:
                 version = row[0]
+
+        if not branch:
+                if version == "3.4":
+                        branch = "3.4"
+                elif version == "3.5":
+                        branch = "3.5"
+                elif version == "3.6":
+                        branch = "3.6"
+                elif version == "3.7":
+                        branch = "3.7"
+                elif version == "3.8":
+                        branch = "3.8"
+                elif version == "devel":
+                        branch = "devel"
 
         date = current_date
 
         if not date:
                 date = dt_parser.parse(row[1])
+
+        number_runs = 5
+
+        if len(row) > 10:
+                number_runs = int(row[10])
 
         print(json.dumps({
                 "test": {
@@ -76,11 +105,11 @@ def simple_performance(row):
                         "min": float(row[5]),
                         "max": float(row[6]),
                         "deviation": float(row[7]),
-                        "numberRuns": int(row[10])
+                        "numberRuns": number_runs
                 },
                 "size": {
                         "collection": row[8],
-                        "count": int(row[9]),
+                        "count": count,
                         "size": size
                 },
                 "configuration": {
@@ -94,11 +123,47 @@ def simple_performance(row):
                 "ms": 1000 * date.timestamp()
         }))
 
+def simple_performance_cluster(row):
+        global version, current_date, branch, mode, edition
+
+        count = row[7]
+        date = current_date
+        name = row[0]
+        number_runs = row[8]
+        size = row[9]
+
+        print(json.dumps({
+                "test": {
+                        "name": name,
+                        "average": float(row[1]),
+                        "median": float(row[2]),
+                        "min": float(row[3]),
+                        "max": float(row[4]),
+                        "deviation": float(row[5]),
+                        "numberRuns": number_runs
+                },
+                "size": {
+                        "collection": row[6],
+                        "count": count,
+                        "size": size
+                },
+                "configuration": {
+                        "version": version,
+                        "branch": branch,
+                        "mode": mode,
+                        "edition": edition
+                },
+                "isoDate": date.isoformat(),
+                "date": date.timestamp(),
+                "ms": 1000 * date.timestamp()
+        }))
 
 with open(options.filename) as csvfile:
         lines = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in lines:
                 if input_type == "simple-performance":
                         simple_performance(row)
+                elif input_type == "simple-performance-cluster":
+                        simple_performance_cluster(row)
                 else:
                         print("unknown output format '%s'" % (input_type))
