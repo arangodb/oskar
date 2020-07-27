008490f03915732fa6f612bc1c9027651f240eb0
Wilfried Goesgens <willi@arangodb.com>
2020-07-27 12:45:11 +0200

    add parameters about test characteristika.

--- jenkins/helper/performance2json.py
+++ jenkins/helper/performance2json.py
@@ -18,12 +18,16 @@ parser.add_option("-e", "--edition", dest="edition", help="community or enterpri
 parser.add_option("-s", "--size", dest="size", help="tiny, small, medium, big", metavar="SIZE")
 parser.add_option("-F", "--type", dest="input_type", help="input format", metavar="TYPE")
 
+parser.add_option("--collectionsPerDatabase", dest="collections_per_database", help="number of collections created in each iteration", metavar="COL_PER_DB")
+parser.add_option("--indexesPerCollection", dest="indexes_per_collection", help="number of indices per collection to be created", metavar="IDX_PER_COL")
+parser.add_option("--numberOfShards", dest="no_shards", help="number of shards created on each collection", metavar="NO_SHARDS")
+parser.add_option("--replicationFactor", dest="replication_factor", help="replication factor on these collections", metavar="REPL_FACTOR")
+
 (options, args) = parser.parse_args()
 
 current_date = None
 headline = []
 values = {}
-values ['aoeu'] = []
 
 if options.date:
         current_date = dt_parser.parse(options.date)
@@ -164,7 +168,7 @@ def simple_performance_cluster(row):
         }))
 
 def ddl_performance_cluster(rownum, row):
-        global version, current_date, branch, mode, edition, headline, values
+        global version, current_date, branch, mode, edition, headline, values, options
 
         if rownum == 0:
                 headline = row
@@ -174,15 +178,25 @@ def ddl_performance_cluster(rownum, row):
                 count = len(values[headline[0]])
                 number_runs = len(values[headline[0]])
                 date = current_date
-                size = 0
 
+                i = 1
+                total_list = values[headline[1]]
+                count_monotonic = 0
+                while i < len(total_list):
+                        if total_list[i] >= total_list[i-1]:
+                                count_monotonic += 1
+                        i += 1
                 result = {
                         "test": {
+                                "name": "ddl"
                         },
+                        "monotonicity": count_monotonic / len(total_list),
                         "size": {
-                                "collection": row[6],
                                 "count": count,
-                                "size": size
+                                "collectionCount": options.collections_per_database,
+                                "indexesPercollection": options.indexes_per_collection,
+                                "numberOfShards": options.no_shards,
+                                "replicationFactor": options.replication_factor
                         },
                         "configuration": {
                                 "version": version,
@@ -195,6 +209,7 @@ def ddl_performance_cluster(rownum, row):
                         "ms": 1000 * date.timestamp()
                 }
 
+                        
                 i = 1
                 while i < len(headline):
                         result['test'][headline[i]] = {
@@ -234,4 +249,4 @@ with open(options.filename) as csvfile:
                         print("unknown output format '%s'" % (input_type))
                 i += 1
         if input_type == "ddl-performance-cluster":
-                ddl_performance_cluster(-1, row)
+                ddl_performance_cluster(-1, [])
