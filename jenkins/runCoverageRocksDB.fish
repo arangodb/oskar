ffccf15de641a857382c6ce3026d2012385449f1
Frank Celler <frank@arangodb.com>
2020-11-01 17:53:01 +0100

    RocksDB only

---
+++ jenkins/runCoverageRocksDB.fish
@@ -0,0 +1,30 @@
+#!/usr/bin/env fish
+source jenkins/helper/jenkins.fish
+
+set s 0
+
+cleanPrepareLockUpdateClear
+and enterprise
+and maintainerOn
+and asanOff
+and coverageOn
+and skipGrey
+and switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
+and set -gx NOSTRIP 1
+and showConfig
+and buildStaticArangoDB -DUSE_FAILURE_TESTS=On -DDEBUG_SYNC_REPLICATION=On -DTARGET_ARCHITECTURE=westmere
+and begin
+  rm -rf $WORKDIR/work/gcov.old
+  if test -d $WORKDIR/work/gcov ; mv $WORKDIR/work/gcov $WORKDIR/work/gcov.old ; end
+
+  rocksdb
+  single     ; oskarFull ; or set s $status
+  cluster    ; oskarFull ; or set s $status
+
+  collectCoverage
+  or set s $status
+end
+or set s $status
+
+cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory 
+exit $s
