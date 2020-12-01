0dc913c31f16eadd779db278d67ae2a58976dac5
Frank Celler <392005+fceller@users.noreply.github.com>
2020-12-01 12:43:03 +0100

    Bug fix/windows (#277)
    * give an understandable error message if TSHARK is missing
    
    * ignore TSHARK

--- helper.psm1
+++ helper.psm1
@@ -18,22 +18,29 @@ If(-Not(Test-Path -PathType Container -Path "work"))
 
 $global:TSHARK = ((Get-ChildItem -ErrorAction SilentlyContinue -Recurse "${env:ProgramFiles}" tshark.exe).FullName | Select-Object -Last 1) -replace ' ', '` '
 
-If((Invoke-Expression "$global:TSHARK -D" | Select-String -SimpleMatch Npcap ) -match '^(\d).*')
+If(-Not($global:TSHARK))
 {
-    $global:dumpDevice = $Matches[1]
-    if ($global:dumpDevice -notmatch '\d+') {
-        Write-Host "unable to detect the loopback-device. we expect this to have an Npcacp one:"
-        Invoke-Expression $global:TSHARK -D
-        Exit 1
-    }
-    Else {
-        $global:TSHARK = $global:TSHARK -replace '` ', ' '
-    }
+    Write-Host "failed to locate TSHARK"
 }
 Else
 {
-    Write-Host "failed to get loopbackdevice - check NCAP Driver installation"
-    Exit 1
+    If((Invoke-Expression "$global:TSHARK -D" | Select-String -SimpleMatch Npcap ) -match '^(\d).*')
+    {
+        $global:dumpDevice = $Matches[1]
+        if ($global:dumpDevice -notmatch '\d+') {
+            Write-Host "unable to detect the loopback-device. we expect this to have an Npcacp one:"
+            Invoke-Expression $global:TSHARK -D
+            Exit 1
+        }
+        Else {
+            $global:TSHARK = $global:TSHARK -replace '` ', ' '
+        }
+    }
+    Else
+    {
+        Write-Host "failed to get loopbackdevice - check NCAP Driver installation"
+        $global:TSHARK = ""
+  }
 }
 
 $global:HANDLE_EXE = $null
