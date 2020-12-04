689fc47fbbe07e1e8c10b404804032b8d3e5fcda
Frank Celler <frank@arangodb.com>
2020-12-04 11:37:26 +0100

    added debugging

--- jenkins/runGuidelinesCheck.fish
+++ jenkins/runGuidelinesCheck.fish
@@ -6,6 +6,8 @@ and eval $EDITION
 and eval $STORAGE_ENGINE
 and eval $TEST_SUITE
 and skipGrey
+and echo ARANGODB_BRANCH: "<$ARANGODB_BRANCH>"
+and echo ENTERPRISE_BRANCH: "<$ENTERPRISE_BRANCH>"
 and switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
 and rm -f work/test.log
 and checkLogId > work/test.log
