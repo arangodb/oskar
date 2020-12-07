5dc6c6b36279a031aaaa6950114bd84d2995f445
Vadim <vadim@arangodb.com>
2020-12-07 20:53:55 +0300

    Add additional occupied port checking

--- jenkins/helper/port.sh
+++ jenkins/helper/port.sh
@@ -26,22 +26,27 @@ find $PORTDIR -type f -cmin +$TIMEOUT -exec rm "{}" ";"
 
 rm -f ./ports
 
+function isFree
+{
+  return $(sudo netstat -tulpn | grep LISTEN | awk '{print $4}' | grep -c ':'$1'$')
+}
+
 case "$1" in
 
   "--cluster")
     shift
-    while ! ((set -o noclobber ; date > $PORTDIR/$port && echo "$PORTDIR/$port" > ./ports &&\
-                                 date > $PORTDIR/`expr $port + 1` && echo "$PORTDIR/`expr $port + 1`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 2` && echo "$PORTDIR/`expr $port + 2`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 3` && echo "$PORTDIR/`expr $port + 3`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 10` && echo "$PORTDIR/`expr $port + 10`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 11` && echo "$PORTDIR/`expr $port + 11`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 12` && echo "$PORTDIR/`expr $port + 12`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 13` && echo "$PORTDIR/`expr $port + 13`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 20` && echo "$PORTDIR/`expr $port + 20`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 21` && echo "$PORTDIR/`expr $port + 21`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 22` && echo "$PORTDIR/`expr $port + 22`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 23` && echo "$PORTDIR/`expr $port + 23`" >> ./ports) 2> /dev/null)
+    while ! ((set -o noclobber ; date > $PORTDIR/$port && isFree $port && echo "$PORTDIR/$port" > ./ports &&\
+                                 date > $PORTDIR/`expr $port + 1` && isFree `expr $port + 1` && echo "$PORTDIR/`expr $port + 1`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 2` && isFree `expr $port + 2` echo "$PORTDIR/`expr $port + 2`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 3` && isFree `expr $port + 3` echo "$PORTDIR/`expr $port + 3`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 10` && isFree `expr $port + 10` echo "$PORTDIR/`expr $port + 10`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 11` && isFree `expr $port + 11` echo "$PORTDIR/`expr $port + 11`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 12` && isFree `expr $port + 12` echo "$PORTDIR/`expr $port + 12`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 13` && isFree `expr $port + 13` echo "$PORTDIR/`expr $port + 13`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 20` && isFree `expr $port + 20` echo "$PORTDIR/`expr $port + 20`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 21` && isFree `expr $port + 21` echo "$PORTDIR/`expr $port + 21`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 22` && isFree `expr $port + 22` echo "$PORTDIR/`expr $port + 22`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 23` && isFree `expr $port + 23` echo "$PORTDIR/`expr $port + 23`" >> ./ports) 2> /dev/null)
     do
       [ -e "./ports" ] && while read -r line; do rm -f "$line"; done < ./ports
       rm -f ./ports
@@ -55,15 +60,15 @@ case "$1" in
 
   "--activefailover")
     shift
-    while ! ((set -o noclobber ; date > $PORTDIR/$port && echo "$PORTDIR/$port" > ./ports &&\
-                                 date > $PORTDIR/`expr $port + 1` && echo "$PORTDIR/`expr $port + 1`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 2` && echo "$PORTDIR/`expr $port + 2`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 10` && echo "$PORTDIR/`expr $port + 10`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 11` && echo "$PORTDIR/`expr $port + 11`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 12` && echo "$PORTDIR/`expr $port + 12`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 20` && echo "$PORTDIR/`expr $port + 20`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 21` && echo "$PORTDIR/`expr $port + 21`" >> ./ports &&\
-                                 date > $PORTDIR/`expr $port + 22` && echo "$PORTDIR/`expr $port + 22`" >> ./ports) 2> /dev/null)
+    while ! ((set -o noclobber ; date > $PORTDIR/$port && isFree $port && echo "$PORTDIR/$port" > ./ports &&\
+                                 date > $PORTDIR/`expr $port + 1` && isFree `expr $port + 1` && echo "$PORTDIR/`expr $port + 1`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 2` && isFree `expr $port + 2` && echo "$PORTDIR/`expr $port + 2`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 10` && isFree `expr $port + 10` && echo "$PORTDIR/`expr $port + 10`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 11` && isFree `expr $port + 11` && echo "$PORTDIR/`expr $port + 11`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 12` && isFree `expr $port + 12` && echo "$PORTDIR/`expr $port + 12`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 20` && isFree `expr $port + 20` && echo "$PORTDIR/`expr $port + 20`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 21` && isFree `expr $port + 21` && echo "$PORTDIR/`expr $port + 21`" >> ./ports &&\
+                                 date > $PORTDIR/`expr $port + 22` && isFree `expr $port + 22` && echo "$PORTDIR/`expr $port + 22`" >> ./ports) 2> /dev/null)
     do
       [ -e "./ports" ] && while read -r line; do rm -f "$line"; done < ./ports
       rm -f ./ports
@@ -76,7 +81,7 @@ case "$1" in
   ;;
 
   "--singleserver")
-    while ! ((set -o noclobber ; date > $PORTDIR/$port && echo "$PORTDIR/$port" > ./ports &&\
+    while ! ((set -o noclobber ; date > $PORTDIR/$port && isFree $port && echo "$PORTDIR/$port" > ./ports &&\
                                  date > $PORTDIR/`expr $port + 1` && echo "$PORTDIR/`expr $port + 1`" >> ./ports) 2> /dev/null)
     do
       [ -e "./ports" ] && while read -r line; do rm -f "$line"; done < ./ports
