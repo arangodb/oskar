d1aa5d78fb9af9145e2c59274626630cfc0e0dad
Frank Celler <frank@arangodb.com>
2020-12-12 09:08:39 +0100

    cleanup of flags

--- scripts/buildAlpine4.fish
+++ scripts/buildAlpine4.fish
@@ -35,7 +35,8 @@ set -g FULLARGS $argv \
  -DUSE_MAINTAINER_MODE=$MAINTAINER \
  -DCMAKE_LIBRARY_PATH=/opt/openssl-$OPENSSL_VERSION/lib \
  -DOPENSSL_ROOT_DIR=/opt/openssl-$OPENSSL_VERSION \
- -DUSE_STRICT_OPENSSL_VERSION=$USE_STRICT_OPENSSL
+ -DUSE_STRICT_OPENSSL_VERSION=$USE_STRICT_OPENSSL \
+ -DUSE_JEMALLOC=$JEMALLOC_OSKAR
 
 if test "$MAINTAINER" = "On"
   set -g FULLARGS $FULLARGS \
@@ -53,13 +54,9 @@ if test "$ASAN" = "On"
 else if test "$COVERAGE" = "On"
   echo "Building with Coverage"
   set -g FULLARGS $FULLARGS \
-    -DUSE_JEMALLOC=$JEMALLOC_OSKAR \
     -DCMAKE_C_FLAGS="$pie -fno-stack-protector -fprofile-arcs -ftest-coverage" \
     -DCMAKE_CXX_FLAGS="$pie -fno-stack-protector -fprofile-arcs -ftest-coverage"
 else
-  set -g FULLARGS $FULLARGS \
-   -DUSE_JEMALLOC=$JEMALLOC_OSKAR
-
   if test "$MAINTAINER" = "On"
     set -g FULLARGS $FULLARGS \
      -DCMAKE_C_FLAGS="$pie -fno-stack-protector" \
