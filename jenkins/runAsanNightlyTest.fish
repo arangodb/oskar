14ac81379dafdec15f950e0a5c34c687471a553d
Wilfried Goesgens <willi@arangodb.com>
2021-01-11 15:35:11 +0100

    revert

--- jenkins/runAsanNightlyTest.fish
+++ jenkins/runAsanNightlyTest.fish
@@ -4,7 +4,7 @@ source jenkins/helper/jenkins.fish
 cleanPrepareLockUpdateClear
 and eval $EDITION
 and eval $STORAGE_ENGINE
-and cluster
+and eval $TEST_SUITE
 and skipGrey
 and switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
 and compiler "$COMPILER_VERSION"
