e41a6aa4c93ed75e0cadfe00b6a59790e2d4b5e8
hkernbach <heiko@arangodb.com>
2021-02-16 22:04:04 +0100

    finalized first working version of our jenkins to github status updater

--- github/updateGitHubStatus.js
+++ github/updateGitHubStatus.js
@@ -1,18 +1,153 @@
-// Variables we need from jenkins env
-const apiKey = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-// Variables that are job specific
-const commitSha = "9ce7f4747557a130c0a19b303bd13d252a121c6a";
-const targetUrl = "https://jenkins.arangodb.biz/test";                                    // TODO: implement url of the job
-const context = "arangodb-matrix-full-pr";                                                // TODO: implement name of the job
-const githubOwner = "";
-const githubRepository = "";
-const descriptionSuccess = "The build succeeded! Ready to be merged after valid review!"; // TODO: implement decsription of the job
-const descriptionPending = "The build started. Waiting ...";
-const descriptionFailure = "The build failed! Check the Details and take care of the failure!";
-const baseUrl = "https://api.github.com/search/issues?q=";
-const completeGetUrl = baseUrl + commitSha;
 const https = require('https');
-const postRequest = (postPath, apiKey, callback, postData) => {
+const fs = require('fs');
+
+const commitStatusResultFileName = 'commitStatusResult.json'
+let commitExtraInformation = {};
+let pullRequestExtraInformation = {
+  url: undefined,
+  info: undefined
+};
+
+// Variables we need from jenkins env:
+const apiToken = process.env.API_TOKEN;       // well, a token :)
+const repository = process.env.REPO;          // Format: arangodb/arangodb
+const targetUrl = process.env.JOB_ID;         // Format: https://www.abc.de/#123
+const actionState = process.env.ACTION_STATE; // States: setPending, setError, setFailure, setSuccess
+const githubBranchName = process.env.ARANGODB_BRANCH;
+
+// error = bool, message = string, extra = object
+const exitAndWriteResultToFile = (error, message, status, extra) => {
+  let result = {
+    error: error,
+    message: message,
+    extra: {}
+  };
+
+  if (!error) {
+    // no error case
+    // commit info area
+    if (commitExtraInformation.hasOwnProperty('commit')) {
+      result.extra.commit = commitExtraInformation.commit;
+    }
+    if (pullRequestExtraInformation.hasOwnProperty('url')) {
+      result.url = pullRequestExtraInformation.url;
+    }
+    if (pullRequestExtraInformation.hasOwnProperty('info')) {
+      result.info = pullRequestExtraInformation.info;
+    }
+    console.log(pullRequestExtraInformation)
+    result.status = 'SUCCESS';
+  } else {
+    // error case
+    if (!status) {
+      // set error as default
+      result.status = "FAIL";
+    } else {
+      result.status = status;
+    }
+  }
+
+  // Note: if we want to supply additional information to file, here is the place (expecting an object)
+  if (typeof extra === 'object') {
+    for (let key in extra) {
+      result.extra[key] = extra[key];
+    }
+  }
+
+  fs.writeFileSync(`./${commitStatusResultFileName}`, JSON.stringify(result, null, 2), 'utf-8');
+
+  if (error) {
+    console.error(message);
+    console.error(`Job is aborted. Wrote details into: ${commitStatusResultFileName}`);
+    process.exit(1);
+  }
+  process.exit(0);
+}
+
+let actionStateIsValid = () => {
+  const validActions = ['setPending', 'setError', 'setFailure', 'setSuccess'];
+  return validActions.includes(actionState);
+};
+if (!actionStateIsValid()) {
+  exitAndWriteResultToFile(true, "No valid JOB_ID env found!");
+}
+if (!apiToken) {
+  exitAndWriteResultToFile(true, "No valid API_TOKEN env found!");
+}
+if (!targetUrl) {
+  exitAndWriteResultToFile(true, "No valid JOB_ID env found!");
+}
+
+// TODO Future: Read the job name from ENV variables
+const jobName = "arangodb-matrix-pr";
+if (!repository) {
+  exitAndWriteResultToFile(true, "No valid REPO env found!", "FAIL_NO_BRANCH");
+}
+const githubOwner = repository.split('/')[0] || "arangodb";
+const githubRepository = repository.split('/')[1] || "arangodb";
+
+// TODO Future: We could add dates to messages here
+const stateToDescriptionMap = {
+  setPending: "The build started. Waiting ...",
+  setSuccess: "The build succeeded! Ready to be merged after valid review!",
+  setFailure: "The build failed! Check the Details and take care of the failure!",
+  setError: "The build errored! Check the Details and take care of the error!"
+};
+
+const stateToGitHubStateMap = {
+  setPending: "pending",
+  setSuccess: "success",
+  setFailure: "failure",
+  setError: "error"
+}
+
+/*
+ * Helper Methods Section Begin
+ */
+
+const erroredFinish = (errorCode, data) => {
+  let message = `Job is not done! We've failed. Error code is: ${errorCode}, got response: ${JSON.stringify(data)}`;
+  exitAndWriteResultToFile(true, message);
+}
+
+let validateResponseStatus = (info) => {
+  let httpErrorCodes = [
+    400, 401, 403, 404, 500, 502, 503, 504
+  ];
+
+  if (httpErrorCodes.includes(info.statusCode)) {
+    return {error: true, code: info.statusCode}
+  }
+
+  return {error: false, code: info.statusCode};
+}
+
+// a bit dirty, but this will be filled first before we have the complete data in evaluatePostStatusResponse
+let responseInformation = undefined;
+
+let evaluatePostStatusResponse = (data) => {
+  if (!responseInformation) {
+    exitAndWriteResultToFile(true, "This is not allowed to happen! Got all data before first response!?");
+  }
+  let result = validateResponseStatus(responseInformation);
+  if (result.error) {
+    erroredFinish(result.code, data);
+  }
+  if (!data) {
+    exitAndWriteResultToFile(true, "We did not receive any data from GitHub!");
+  }
+
+  // at this point we do have a success (data + positive status code)
+  console.info(`Job is done. GitHub Status is updated. Wrote result into: ${commitStatusResultFileName}`);
+  exitAndWriteResultToFile(false, `Properly set status to: ${actionState}`);
+
+  // TODO Future Idea: We can also add modify labels: e.g. "Jenkins approved", "Jenkins failed" etc.
+}
+
+const postStatus = (postData, sha) => {
+  const postPath = `/repos/${githubOwner}/${githubRepository}/statuses/${sha}`;
+  let responseData = ''; // placeholder for our reponse data chunks
+
   postData = JSON.stringify(postData);
   let options = {
     hostname: 'api.github.com',
@@ -20,29 +155,40 @@ const postRequest = (postPath, apiKey, callback, postData) => {
     path: postPath,
     method: 'POST',
     headers: {
-      'authorization': 'token ' + apiKey,
+      'authorization': 'token ' + apiToken,
       'user-agent': 'Awesome-Octocat-App',
       'Accept': 'application/vnd.github.v3+json',
       'Content-Length': postData.length,
       'Content-Type': 'application/json'
     }
   };
-  console.log(options);
-  let req = https.request(options, (res) => {
-    res.on('data', (d) => {
-      process.stdout.write(d);
+
+  let req = https.request(options, (response) => {
+    response.on('error', (e) => {
+      exitAndWriteResultToFile(true, JSON.stringify(e));
+    });
+    response.on('end', () => {
+      try {
+        evaluatePostStatusResponse(JSON.parse(responseData));
+      } catch (e) {
+        exitAndWriteResultToFile(true, JSON.stringify(e));
+      }
     });
   });
-  req.on('error', (e) => {
-    console.error(e);
-    console.log("Was not able to update status .. (this is not critical)");
-    // TODO: mark update jenkins job as failed (this should not be critical)
+
+  req.on('response', response => {
+    responseInformation = response;
+    response.on('data', chunk => {
+      responseData += chunk;
+    });
   });
+
   req.write(postData);
   req.end();
 };
-const getRequest = (url, apiKey, callback) => {
-  // api key here currently unused (as not needed by api here)
+
+const getRequest = (urlSuffix, callback, extraCallbackArgument) => {
+  let url = 'https://api.github.com' + urlSuffix;
   let options = {
     headers: {
       'user-agent': 'Awesome-Octocat-App'
@@ -54,38 +200,117 @@ const getRequest = (url, apiKey, callback) => {
       data += chunk;
     });
     resp.on('end', () => {
-      callback(data);
+      if (extraCallbackArgument) {
+        callback(data, extraCallbackArgument);
+      } else {
+        callback(data);
+      }
     });
   }).on("error", (err) => {
-    // In general - in cause of failure we will just not report to the GitHub PR at all
-    // But this should not happen in general of course.
-    console.log(err);
-    console.log("Was not able to update status .. (this is not critical)");
-    // TODO: mark update jenkins job as failed (this should not be critical)
+    exitAndWriteResultToFile(true, `Failed to fetch commits from branch: ${githubBranchName}, tried URL: ${url}`);
   });
 };
-const callbackPost = (data) => {
-  console.log("Finished post request, running callback now: ");
-  console.log(JSON.parse(data));
-}
-// Main code starts here
-const callbackGet = (data) => { // TODO: check if we can get rid of the GET req.
-  let parsedGetResponse = JSON.parse(data);
-  let htmlUrl = parsedGetResponse.items[0].html_url;
-  let htmlUrlSplit = htmlUrl.split('/');
-  let owner = htmlUrlSplit[3]; // TODO: check if we can extract from jenkins
-  let repo = htmlUrlSplit[4]; // TODO: check if we can extract from jenkins
-  let postPath = `/repos/${owner}/${repo}/statuses/${commitSha}`;
-  let resultState = "failure"; // can be error, failure, pending, or success.
-  let postData = {
-    state: resultState,
+
+const buildPayload = () => {
+  return payload = {
+    state: stateToGitHubStateMap[actionState],
     target_url: targetUrl,
-    description: descriptionSuccess,
-    context: context
+    description: stateToDescriptionMap[actionState],
+    context: jobName
   };
-  postRequest(postPath, apiKey, callbackPost, postData);
+};
+
+/*
+ * Helper Methods Section End
+ */
+
+const checkPRMethod = (prCheckData, sha) => {
+  try {
+    prCheckData = JSON.parse(prCheckData);
+  } catch (e) {
+    exitAndWriteResultToFile(true, "Could not parse server response: " + JSON.stringify(e));
+  }
+
+  // bool to check whether we won't find expected attributes we need
+  let parseError = false;
+
+  if (prCheckData.hasOwnProperty('total_count')) {
+    if (prCheckData.total_count >= 1) {
+      let infoItem = prCheckData.items[0];
+      if (infoItem.hasOwnProperty('body')) {
+        // just additional information, let's not fail here
+        pullRequestExtraInformation.info = infoItem.body;
+      }
+
+      if (infoItem.hasOwnProperty('pull_request')) {
+        let pr = infoItem.pull_request;
+        if (pr.hasOwnProperty('url')) {
+          pullRequestExtraInformation.url = infoItem.pull_request.url;
+        } else {
+          parseError = true;
+        }
+      } else {
+        parseError = true;
+      }
+
+      if (parseError) {
+        exitAndWriteResultToFile(true, "Could not parse expected attributes. Format might have been changed.");
+      }
+
+      // if all good and PR found
+      continueWithAction(sha);
+    } else {
+      exitAndWriteResultToFile(true, "Could not read pull request details from GitHub Search API");
+    }
+  } else {
+    // TODO Future: This is no actual error, we just do not need to continue with that script as it would be useless.
+    // Check how to handle this case in the future.
+    exitAndWriteResultToFile(true, "Found no related pull request. Exiting. Nothing to do.", 'FAIL_NO_PR');
+  }
 }
-// First, get the information we need (this may be dropped if we are able to READ
-// 1.) REPO 2.) OWNER from Jenkins
-getRequest(completeGetUrl, null, callbackGet);
-// CallbackGet will then build and POST to GitHub the commit status
+
+const checkPRExists = (sha) => {
+  const checkPRExistencePath = `/search/issues?q=${sha}`;
+  getRequest(checkPRExistencePath, checkPRMethod, sha);
+}
+
+// Note: supplied sha must be valid here.
+const continueWithAction = (sha) => {
+  let postData = buildPayload();
+  postStatus(postData, sha);
+}
+
+// Method to first initialize our specific sha parameter
+const getCommitSha = () => {
+  if (!githubBranchName) {
+    exitAndWriteResultToFile(true, "No valid ARANGODB_BRANCH env found!");
+  }
+  // read branch name and find last commit SHA ID
+  // Example: https://api.github.com/repos/arangodb/arangodb/commits?sha=bug-fix%2Fdevsup-720
+
+  let getCommitShaUrl = `/repos/${githubOwner}/${githubRepository}/commits?sha=${encodeURIComponent(githubBranchName)}`;
+  getRequest(getCommitShaUrl, (data) => {
+    if (data) {
+      try {
+        data = JSON.parse(data);
+        commitExtraInformation = data[0];
+        let sha = data[0].sha; // as only last item + sha is out of interest here
+        if (!sha) {
+          exitAndWriteResultToFile(true, "Could not parse commit SHA information!");
+        }
+
+        // We now do have all information we need, either supplied via Jenkins Environment or this GitHub API
+        checkPRExists(sha);
+      } catch (e) {
+        exitAndWriteResultToFile(true, JSON.stringify(e));
+      }
+    }
+  });
+}
+
+// Actual program flow logic starts here => Will continue with inner callbacks
+// Reason: Native https module does not support await/async. We don't want to include other dependencies here.
+getCommitSha();
+
+// Anyway, to get anyone a better starting point in the future (flow in case of success):
+// getCommitSha() => continueWithAction(<sha>) => postStatus(<preparedPayload>)
