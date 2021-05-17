b70e21f2edc9bb2e1c5a3207ade1fbd66124b4b0
Vadim Kondratyev <vadim@arangodb.com>
2021-05-17 22:16:04 +0200

    Add buildExamplesCoverage.fish job

---
+++ jenkins/buildExamplesCoverage.fish
@@ -0,0 +1,26 @@
+#!/usr/bin/env fish
+source jenkins/helper/jenkins.fish
+
+cleanPrepareLockUpdateClear
+and enterprise
+and maintainerOn
+and rocksdb
+and asanOff
+and coverageOn
+and switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
+and set -gx NOSTRIP 1
+and showConfig
+and buildStaticArangoDB -DUSE_FAILURE_TESTS=On -DDEBUG_SYNC_REPLICATION=On
+and eval $WORKDIR/work/ArangoDB/build/bin/arangod --version > $WORKDIR/work/version.txt
+and begin
+  rm -rf $WORKDIR/work/gcov.old
+  if test -d $WORKDIR/work/gcov ; mv $WORKDIR/work/gcov $WORKDIR/work/gcov.old ; end
+  buildExamples
+  and collectCoverage
+  and mv $WORKDIR/work/version.txt $WORKDIR/work/coverage/version.txt
+  or set s $status
+end
+or set s $status
+
+cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory
+exit $s
