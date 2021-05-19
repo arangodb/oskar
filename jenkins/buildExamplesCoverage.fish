d140af44636862dc7e4dd1f7c7994c92cc7034da
Vadim Kondratyev <vadim@arangodb.com>
2021-05-19 13:44:41 +0200

    Fix double building

--- jenkins/buildExamplesCoverage.fish
+++ jenkins/buildExamplesCoverage.fish
@@ -12,6 +12,7 @@ and switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
 and set -gx NOSTRIP 1
 and showConfig
 and buildStaticArangoDB -DUSE_FAILURE_TESTS=On -DDEBUG_SYNC_REPLICATION=On
+and keepBuild
 and eval $WORKDIR/work/ArangoDB/build/bin/arangod --version > $WORKDIR/work/version.txt
 and begin
   rm -rf $WORKDIR/work/gcov.old
