0aeb5a1d5bd158861e0fd99e1a2ef63d9a6e7bf3
Manuel Pöter <manuel.poeter@leanfive.com>
2021-07-14 14:36:15 +0200

    Add chaos test suite to tests. (#328)
    * Add chaos test suite to tests.
    
    * Update runFullTests.ps1
    
    * Update runFullTests.fish
    
    * Update runFullTests.fish
    
    * Update runFullTests.fish
    
    Co-authored-by: Vadim <vadim@arangodb.com>

--- scripts/runTests.ps1
+++ scripts/runTests.ps1
@@ -128,6 +128,7 @@ Function global:registerClusterTests()
     registerTest -cluster $true -testname "ssl_server"
     registerTest -cluster $true -testname "audit_client"
     registerTest -cluster $true -testname "audit_server"
+    registerTest -cluster $true -testname "chaos"
     # Note that we intentionally do not register the hot_backup test here,
     # since it is currently not supported on Windows. The reason is that
     # the testing framework does not support automatic restarts of instances
