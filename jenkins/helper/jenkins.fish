b9d0328262ecef93eb568bfdf2aba655e9785d07
Vadim Kondratyev <vadim@arangodb.com>
2021-10-13 21:24:03 +0200

    Fix initial oskar presence check

--- jenkins/helper/jenkins.fish
+++ jenkins/helper/jenkins.fish
@@ -20,7 +20,7 @@ function prepareOskar
 
   git config --global http.postBuffer 524288000
   and git config --global https.postBuffer 524288000
-  and if not cd $OSKAR > /dev/null
+  and if not test -d $OSKAR
     echo clone --progress  -b $OSKAR_BRANCH ssh://git@github.com/arangodb/oskar $OSKAR
     git clone --progress  -b $OSKAR_BRANCH ssh://git@github.com/arangodb/oskar $OSKAR ; and cd $OSKAR
   else
