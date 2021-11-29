43a3546c5088a3ebd6fabaea6643126d003916a1
Frank Celler <frank@arangodb.com>
2021-11-29 13:38:52 +0100

    use iso date time

--- jenkins/helper/runCompilerPerformance.fish
+++ jenkins/helper/runCompilerPerformance.fish
@@ -18,8 +18,9 @@ and buildStaticArangoDB
 
 set -l s $status
 set -l filename work/compiler.csv
+set -l iso (echo $datetime | awk '{print substr($0,1,4) "-" substr($0,5,2) "-" substr($0,7,2) "T" substr($0,9,2) ":" substr($0,11,2) ":00" }')
 
 echo "branch,os,date,step,runtime" > $filename
-awk -F, "{print \"$ARANGODB_BRANCH,$OS,$date,\" \$2 \",\" \$3}" \
+awk -F, "{print \"$ARANGODB_BRANCH,$OS,$iso,\" \$2 \",\" \$3}" \
   < work/buildTimes.csv \
   >> $filename
