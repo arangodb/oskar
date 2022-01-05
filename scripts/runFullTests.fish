04b84e7f58e485de5313da6e2b1595dd79518ef4
Lars Maier <lars@arangodb.com>
2022-01-05 17:42:33 +0100

    Added replication2_server suite. (#370)
    * Added replication2_server suite.
    
    * Update scripts/runFullTests.fish
    
    Co-authored-by: Vadim <vadim@arangodb.com>
    
    * Update scripts/runFullTests.fish
    
    Co-authored-by: Vadim <vadim@arangodb.com>

--- scripts/runFullTests.fish
+++ scripts/runFullTests.fish
@@ -102,6 +102,7 @@ set ST "$ST""500,runSingleTest1 paths_server -\n"
 set ST "$ST""250,runSingleTest1 hot_backup -\n"
 # The suite doesn't have single-server tests (yet)
 #set ST "$ST""250,runSingleTest1 replication2_client -\n"
+#set ST "$ST""250,runSingleTest1 replication2_server -\n"
 
 set -g STS (echo -e $ST | fgrep , | sort -rn | awk -F, '{print $2}')
 set -g STL (count $STS)
@@ -217,6 +218,7 @@ set CT "$CT""250,runClusterTest1 hot_backup - --dumpAgencyOnError true\n"
 set CT "$CT""250,runClusterTest1 arangobench - --dumpAgencyOnError true\n"
 set CT "$CT""9600,runClusterTest1 chaos - --dumpAgencyOnError true --skipNightly false\n"
 set ST "$ST""250,runSingleTest1 replication2_client -\n"
+set ST "$ST""250,runSingleTest1 replication2_server - --dumpAgencyOnError true\n"
 
 set -g CTS (echo -e $CT | fgrep , | sort -rn | awk -F, '{print $2}')
 set -g CTL (count $CTS)
--- scripts/runTests.ps1
+++ scripts/runTests.ps1
@@ -76,6 +76,7 @@ Function global:registerSingleTests()
     registerTest -testname "permissions"
     registerTest -testname "paths_server"
 #    registerTest -testname "replication2_client"
+#    registerTest -testname "replication2_server"
     # Note that we intentionally do not register the hot_backup test here,
     # since it is currently not supported on Windows. The reason is that
     # the testing framework does not support automatic restarts of instances
@@ -133,6 +134,7 @@ Function global:registerClusterTests()
     registerTest -cluster $true -testname "audit_server"
     registerTest -cluster $true -testname "chaos"
     registerTest -cluster $true -testname "replication2_client"
+    registerTest -cluster $true -testname "replication2_server"
     # Note that we intentionally do not register the hot_backup test here,
     # since it is currently not supported on Windows. The reason is that
     # the testing framework does not support automatic restarts of instances
