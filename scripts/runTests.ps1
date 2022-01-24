259eb6469c80e34b145c0bf393c99563ae1fe82e
Wilfried Goesgens <willi@arangodb.com>
2022-01-24 16:52:11 +0100

    enable v8 trace logging for this testsuite to better trap crashes (#371)
    * enable v8 trace logging for this testsuite to better trap crashes
    
    * Update runFullTests.ps1
    
    * Update runTests.ps1
    
    * quotes
    
    * Update runFullTests.ps1
    
    * Update runTests.ps1
    
    Co-authored-by: Vadim <vadim@arangodb.com>

--- scripts/runTests.ps1
+++ scripts/runTests.ps1
@@ -108,7 +108,7 @@ Function global:registerClusterTests()
     registerTest -cluster $true -testname "dump_encrypted"
     registerTest -cluster $true -testname "dump_with_crashes"
     registerTest -cluster $true -testname "export"
-    registerTest -cluster $true -testname "http_server"  -sniff true
+    registerTest -cluster $true -testname "http_server" -sniff true -moreParams "--extraArgs:log.level v8=trace"
     registerTest -cluster $true -testname "importing"
     registerTest -cluster $true -testname "server_secrets"
     registerTest -cluster $true -testname "server_permissions"
@@ -123,7 +123,7 @@ Function global:registerClusterTests()
     registerTest -cluster $true -testname "recovery_cluster" -index "2" -bucket "4/2" "arangosearch"
     registerTest -cluster $true -testname "recovery_cluster" -index "3" -bucket "4/3" "arangosearch"
     registerTest -cluster $true -testname "shell_client"
-    registerTest -cluster $true -testname "shell_server_aql" -index "0" -bucket "5/0"
+    registerTest -cluster $true -testname "shell_server_aql" -index "0" -bucket "5/0" -moreParams "--extraArgs:log.level v8=trace"
     registerTest -cluster $true -testname "shell_server_aql" -index "1" -bucket "5/1"
     registerTest -cluster $true -testname "shell_server_aql" -index "2" -bucket "5/2"
     registerTest -cluster $true -testname "shell_server_aql" -index "3" -bucket "5/3"
