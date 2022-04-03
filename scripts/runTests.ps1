dae5546cc8d29b101ab44e9a2bbd5e979c140bd0
maierlars <lars@arangodb.com>
2022-04-03 15:33:04 +0200

    Use invoke expression.

--- scripts/runTests.ps1
+++ scripts/runTests.ps1
@@ -14,8 +14,7 @@ Function global:registerSingleTests()
 
     if (Test-Path "$INNERWORKDIR/ArangoDB/tests/test-definitions.txt" -PathType Leaf) {
         Write-Host "Using test definitions from repo..."
-        $tests = C:\Python310\python.exe $INNERWORKDIR/ArangoDB/scripts/generateJenkinsScripts.py $INNERWORKDIR/ArangoDB/tests/test-definitions.txt -f ps1
-        $ExecutionContext.InvokeCommand.ExpandString($tests)
+        C:\Python310\python.exe $INNERWORKDIR/ArangoDB/scripts/generateJenkinsScripts.py $INNERWORKDIR/ArangoDB/tests/test-definitions.txt -f ps1 | Invoke-Expression
     } else {
         registerTest -testname "upgrade_data_3.2.*"
         registerTest -testname "upgrade_data_3.3.*"
@@ -104,8 +103,7 @@ Function global:registerClusterTests()
 
     if (Test-Path "$INNERWORKDIR/ArangoDB/tests/test-definitions.txt" -PathType Leaf) {
         Write-Host "Using test definitions from repo..."
-        $tests = C:\Python310\python.exe $INNERWORKDIR/ArangoDB/scripts/generateJenkinsScripts.py $INNERWORKDIR/ArangoDB/tests/test-definitions.txt -f ps1 --cluster
-        $ExecutionContext.InvokeCommand.ExpandString($tests)
+        C:\Python310\python.exe $INNERWORKDIR/ArangoDB/scripts/generateJenkinsScripts.py $INNERWORKDIR/ArangoDB/tests/test-definitions.txt -f ps1 --cluster | Invoke-Expression
     } else {
         registerTest -cluster $true -testname "load_balancing"
         registerTest -cluster $true -testname "load_balancing_auth"
