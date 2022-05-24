c5985330c1ca53afe38d11e4dd62dbbe90b3568a
Frank Celler <frank@arangodb.com>
2022-05-24 23:53:49 +0200

    fixed path

--- scripts/rlog/pr.ps1
+++ scripts/rlog/pr.ps1
@@ -1,5 +1,4 @@
-Import-Module "$PSScriptRoot\lib\Utils.psm1"
-
+Import-Module "$PSScriptRoot\..\lib\Utils.psm1"
 Import-Module "$global:ARANGODIR\tests\Definition\rlog\pr.psm1"
 
 $global:TESTSUITE = "tests"
