6f0b17e574ad678f233490512b3f5e32b41dfa54
Wilfried Goesgens <willi@arangodb.com>
2022-07-12 17:37:21 +0200

    fix workspace handling

--- jenkins/helper/generate_jenkins_scripts.py
+++ jenkins/helper/generate_jenkins_scripts.py
@@ -41,10 +41,18 @@ if sys.version_info[0] != 3:
 
 def get_workspace():
     """ evaluates the directory to put reports to """
+    if 'WORKSPACE' in os.environ:
+        p = Path(os.environ['WORKSPACE'])
+        if p.exists():
+            return p
     if 'WORKDIR' in os.environ:
-        return Path(os.environ['WORKDIR'])
+        p = Path(os.environ['WORKDIR'])
+        if p.exists():
+            return p
     if 'INNERWORKDIR' in os.environ:
-        return Path(os.environ['INNERWORKDIR'])
+        p = Path(os.environ['INNERWORKDIR'])
+        if p.exists():
+            return p
     return Path.cwd() / 'work'
 
 class TestConfig():
