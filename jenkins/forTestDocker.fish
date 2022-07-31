9b403e0ac08b45f7729140f273dfb0fc36e10ec3
Vadim Kondratyev <vadim@arangodb.com>
2022-07-31 15:40:26 +0200

    Fix test docker creation

--- jenkins/forTestDocker.fish
+++ jenkins/forTestDocker.fish
@@ -4,26 +4,27 @@ source jenkins/helper/jenkins.fish
 if test -z "$DOCKER_TAG"
   echo "DOCKER_TAG required"
   exit 1
+else
+  set -xg DOCKER_TAG_JENKINS "$DOCKER_TAG"
 end
 
-if test "$USE_ARM" = "On"
-  switch "$ARCH"
-    case "x86_64"
-      set archSuffix "-amd64"
-    case '*'
-      if string match --quiet --regex '^arm64$|^aarch64$' $ARCH >/dev/null
-      set archSuffix "-arm64v8"
-    else
-      echo "fatal, unknown architecture $ARCH for docker"
-      exit 1
+set archSuffix ""
+
+function setArchSuffix
+  if test "$USE_ARM" = "On"
+    switch "$ARCH"
+      case "x86_64"
+        set archSuffix "-amd64"
+      case '*'
+        if string match --quiet --regex '^arm64$|^aarch64$' $ARCH >/dev/null
+        set archSuffix "-arm64v8"
+      else
+        echo "fatal, unknown architecture $ARCH for docker"
+        exit 1
+      end
     end
   end
 end
-
-set TAG $DOCKER_TAG$archSuffix
-
-set -xg HUB_COMMUNITY "arangodb/arangodb-test:$TAG"
-set -xg HUB_ENTERPRISE "arangodb/enterprise-test:$TAG"
 
 cleanPrepareLockUpdateClear
 and rm -rf $WORKSPACE/imagenames.log
@@ -32,6 +33,8 @@ and switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
 and findArangoDBVersion
 and buildStaticArangoDB
 and downloadStarter
+and setArchSuffix
+and set -xg HUB_COMMUNITY "arangodb/arangodb-test:$DOCKER_TAG_JENKINS$archSuffix"
 and buildDockerImage $HUB_COMMUNITY
 and docker push $HUB_COMMUNITY
 and docker tag $HUB_COMMUNITY $GCR_REG_PREFIX$HUB_COMMUNITY
@@ -52,6 +55,8 @@ and buildStaticArangoDB
 and downloadStarter
 and downloadSyncer
 and copyRclone "linux"
+and setArchSuffix
+and set -xg HUB_ENTERPRISE "arangodb/enterprise-test:$$DOCKER_TAG_JENKINS$archSuffix"
 and buildDockerImage $HUB_ENTERPRISE
 and docker push $HUB_ENTERPRISE
 and docker tag $HUB_ENTERPRISE $GCR_REG_PREFIX$HUB_ENTERPRISE
@@ -62,4 +67,3 @@ and echo $GCR_REG_PREFIX$HUB_ENTERPRISE >> $WORKSPACE/imagenames.log
 set -l s $status
 cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory
 exit $s
-
--- jenkins/forTestDockerEnterprise.fish
+++ jenkins/forTestDockerEnterprise.fish
@@ -4,24 +4,27 @@ source jenkins/helper/jenkins.fish
 if test -z "$DOCKER_TAG"
   echo "DOCKER_TAG required"
   exit 1
+else
+  set -xg DOCKER_TAG_JENKINS "$DOCKER_TAG"
 end
 
-if test "$USE_ARM" = "On"
-  switch "$ARCH"
-    case "x86_64"
-      set archSuffix "-amd64"
-    case '*'
-      if string match --quiet --regex '^arm64$|^aarch64$' $ARCH >/dev/null
-      set archSuffix "-arm64v8"
-    else
-      echo "fatal, unknown architecture $ARCH for docker"
-      exit 1
+set archSuffix ""
+
+function setArchSuffix
+  if test "$USE_ARM" = "On"
+    switch "$ARCH"
+      case "x86_64"
+        set archSuffix "-amd64"
+      case '*'
+        if string match --quiet --regex '^arm64$|^aarch64$' $ARCH >/dev/null
+        set archSuffix "-arm64v8"
+      else
+        echo "fatal, unknown architecture $ARCH for docker"
+        exit 1
+      end
     end
   end
 end
-
-set TAG $DOCKER_TAG$archSuffix
-set -xg HUB_ENTERPRISE "arangodb/enterprise-test:$TAG"
 
 cleanPrepareLockUpdateClear
 and rm -rf $WORKSPACE/imagenames.log
@@ -32,6 +35,8 @@ and buildStaticArangoDB
 and downloadStarter
 and downloadSyncer
 and copyRclone "linux"
+and setArchSuffix
+and set -xg HUB_ENTERPRISE "arangodb/enterprise-test:$$DOCKER_TAG_JENKINS$archSuffix"
 and buildDockerImage $HUB_ENTERPRISE
 and docker push $HUB_ENTERPRISE
 and docker tag $HUB_ENTERPRISE $GCR_REG_PREFIX$HUB_ENTERPRISE
