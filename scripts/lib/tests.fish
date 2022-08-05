c361374e0f315d42efc413fce3b9a768fc4ac538
Valery Mironov <32071355+MBkkt@users.noreply.github.com>
2022-08-05 09:55:26 +0300

    Update tests.fish

--- scripts/lib/tests.fish
+++ scripts/lib/tests.fish
@@ -56,7 +56,7 @@ if not test -z $SAN; and test $SAN = "On"
       echo "UBSAN: $UBSAN_OPTIONS"
     case "TSan"
       # thread sanitizer
-      set -xg TSAN_OPTIONS "$common_options:log_path=/work/tsan.log:detect_deadlocks=true:second_deadlock_stack=1"
+      set -xg TSAN_OPTIONS "$common_options:symbolize=0:log_path=/work/tsan.log:detect_deadlocks=true:second_deadlock_stack=1"
 
       # suppressions
       if test -f $INNERWORKDIR/ArangoDB/tsan_arangodb_suppressions.txt
