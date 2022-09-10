8237e044d897cdce5f40c1d807045ef571b4922f
Wilfried Goesgens <willi@arangodb.com>
2022-09-10 10:14:22 +0200

    print cpu etc. temperatures

--- jenkins/helper/test_launch_controller.py
+++ jenkins/helper/test_launch_controller.py
@@ -351,6 +351,7 @@ class SiteConfig:
  - {self.available_slots} test slots
  - {str(TEMP)} - temporary directory
  - current Disk I/O: {str(psutil.disk_io_counters())}
+ - temperatures: {str(psutil.sensors_temperatures())}
 """)
         self.cfgdir = base_source_dir / 'etc' / 'relative'
         self.bin_dir = bin_dir
@@ -448,7 +449,8 @@ class TestingRunner():
             print(str(psutil.getloadavg()) + "<= Load " +
                   "Running: " + str(self.running_suites) +
                   " => Active Slots: " + str(self.used_slots) +
-                  " => Disk I/O: " + str(psutil.disk_io_counters()))
+                  " => Disk I/O: " + str(psutil.disk_io_counters()) +
+                  " => Temperatures: " + str(psutil.sensors_temperatures()))
         sys.stdout.flush()
 
     def done_job(self, parallelity):
@@ -471,7 +473,8 @@ class TestingRunner():
         if ((load[0] > self.cfg.max_load) or
             (load[1] > self.cfg.max_load1)):
             print(F"{str(load)} <= Load to high; waiting before spawning more - Disk I/O: " +
-                  str(psutil.disk_io_counters()))
+                  str(psutil.disk_io_counters()) +
+                  "Temperatures: " + str(psutil.sensors_temperatures()))
             return False
         with self.slot_lock:
             self.used_slots += self.scenarios[offset].parallelity
