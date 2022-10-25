ea54c628afa07ced87d1d6569b35b2072dbc59dd
Wilfried Goesgens <willi@arangodb.com>
2022-10-25 20:20:54 +0200

    work around macos root requirement

--- jenkins/helper/test_launch_controller.py
+++ jenkins/helper/test_launch_controller.py
@@ -543,21 +543,41 @@ def testing_runner(testing_instance, this, arangosh):
 def get_socket_count():
     """ get the number of sockets lingering destruction """
     counter = 0
-    for socket in psutil.net_connections(kind='inet'):
-        if socket.status in [
-                psutil.CONN_FIN_WAIT1,
-                psutil.CONN_FIN_WAIT2,
-                psutil.CONN_CLOSE_WAIT,
-                psutil.CONN_ESTABLISHED,
-                psutil.CONN_SYN_SENT,
-                psutil.CONN_SYN_RECV,
-                psutil.CONN_TIME_WAIT,
-                psutil.CONN_CLOSE,
-                psutil.CONN_LAST_ACK,
-                psutil.CONN_LISTEN,
-                psutil.CONN_CLOSING
-        ]:
-            counter += 1
+    if IS_MAC:
+        for proc in psutil.process_iter(['pid', 'name']):
+            if proc.name != 'arangod':
+                continue
+            for socket in psutil.Process(proc.pid).connections():
+                if socket.status in [
+                        psutil.CONN_FIN_WAIT1,
+                        psutil.CONN_FIN_WAIT2,
+                        psutil.CONN_CLOSE_WAIT,
+                        psutil.CONN_ESTABLISHED,
+                        psutil.CONN_SYN_SENT,
+                        psutil.CONN_SYN_RECV,
+                        psutil.CONN_TIME_WAIT,
+                        psutil.CONN_CLOSE,
+                        psutil.CONN_LAST_ACK,
+                        psutil.CONN_LISTEN,
+                        psutil.CONN_CLOSING
+                ]:
+                    counter += 1
+    else:
+        for socket in psutil.net_connections(kind='inet'):
+            if socket.status in [
+                    psutil.CONN_FIN_WAIT1,
+                    psutil.CONN_FIN_WAIT2,
+                    psutil.CONN_CLOSE_WAIT,
+                    psutil.CONN_ESTABLISHED,
+                    psutil.CONN_SYN_SENT,
+                    psutil.CONN_SYN_RECV,
+                    psutil.CONN_TIME_WAIT,
+                    psutil.CONN_CLOSE,
+                    psutil.CONN_LAST_ACK,
+                    psutil.CONN_LISTEN,
+                    psutil.CONN_CLOSING
+            ]:
+                counter += 1
     return counter
 
 class TestingRunner():
