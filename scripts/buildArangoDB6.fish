5b04a11843ecda42914b83fb7119ec4358f2895f
Vadim <vadim@arangodb.com>
2022-11-14 16:11:11 +0100

    Switch all sanitizers builds to clang (#458)

--- scripts/buildArangoDB6.fish
+++ scripts/buildArangoDB6.fish
@@ -64,13 +64,13 @@ end
 #end
 
 if test "$SAN" = "On"
+  set -xg CC_NAME clang
+  set -xg CXX_NAME clang++
   # Suppress leaks detection only during building
   set -gx SAN_OPTIONS "detect_leaks=0"
   set -l SANITIZERS "-fsanitize=address -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=leak"
   if test "$SAN_MODE" = "TSan"
     set SANITIZERS "-fsanitize=thread"
-    set -xg CC_NAME clang
-    set -xg CXX_NAME clang++
   end
   set -g FULLARGS $FULLARGS \
    -DUSE_JEMALLOC=Off \
