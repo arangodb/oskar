45abb4f24b625a9d766faf746dc59cad0c2506ee
Wilfried Goesgens <willi@arangodb.com>
2022-11-24 01:51:21 +0100

    for the wintendo we need to use more force to get rid of stuck arango processes (#464)
    * for the wintendo we need to use more force to get rid of stuck arango processes
    
    * is method
    
    * fix function

--- jenkins/helper/tools/killall.py
+++ jenkins/helper/tools/killall.py
@@ -22,3 +22,21 @@ def list_all_processes():
         print(f"{process.pid} {cmdline}")
     print(pseaf)
     sys.stdout.flush()
+
+def kill_all_arango_processes():
+    """list all processes for later reference"""
+    pseaf = "PID  Process"
+    # pylint: disable=catching-non-exception
+    for process in psutil.process_iter(["pid", "name"]):
+        if process.name().lower().find('arango') >= 0:
+            try:
+                print(f"Main: killing {process.name()} - {str(process.pid)}")
+                process.resume()
+            except psutil.NoSuchProcess:
+                pass
+            except psutil.AccessDenied:
+                pass
+            try:
+                process.kill()
+            except psutil.NoSuchProcess:  # pragma: no cover
+                pass
