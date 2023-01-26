d5403d39ac002861c0aaa9aa1c47048241793b36
Wilfried Goesgens <willi@arangodb.com>
2023-01-26 14:22:09 +0100

    fix syntax

--- scripts/runTests.ps1
+++ scripts/runTests.ps1
@@ -19,7 +19,7 @@ Function global:registerSingleTests()
 
     Write-Host "Using test definitions from repo..."
     pip install py7zr
-    proc -process "python.exe" -argument "$env:WORKSPACE\jenkins\helper\test_launch_controller.py $INNERWORKDIR\ArangoDB\tests\test-definitions.txt" -logfile $false -priority "Normal" $$ENTERPRISE_ARG
+    proc -process "python.exe" -argument "$env:WORKSPACE\jenkins\helper\test_launch_controller.py $INNERWORKDIR\ArangoDB\tests\test-definitions.txt" -logfile $false -priority "Normal" $ENTERPRISE_ARG
 }
 
 Function global:registerClusterTests()
@@ -36,7 +36,7 @@ Function global:registerClusterTests()
 
     Write-Host "Using test definitions from repo..."
     pip install py7zr
-    proc -process "python.exe" -argument "$env:WORKSPACE\jenkins\helper\test_launch_controller.py $INNERWORKDIR\ArangoDB\tests\test-definitions.txt --cluster" -logfile $false -priority "Normal"  $$ENTERPRISE_ARG
+    proc -process "python.exe" -argument "$env:WORKSPACE\jenkins\helper\test_launch_controller.py $INNERWORKDIR\ArangoDB\tests\test-definitions.txt --cluster" -logfile $false -priority "Normal"  $ENTERPRISE_ARG
 }
 
 runTests
