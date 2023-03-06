9efb1d4ea7b41e8f984c7eddcb46f8cdb0fab3ef
Wilfried Goesgens <willi@arangodb.com>
2023-03-06 10:30:41 +0100

    use

--- scripts/coverage.fish
+++ scripts/coverage.fish
@@ -2,46 +2,46 @@
 set -l c 0
 
 cd $INNERWORKDIR
-and rm -rf /work/combined
-and mkdir /work/combined
-and python3 "$WORKSPACE/jenkins/helper/aggregate_coverage.py" /work/gcov /work/combined/result
+and rm -rf $INNERWORKDIR/combined
+and mkdir $INNERWORKDIR/combined
+and python3 "$WORKSPACE/jenkins/helper/aggregate_coverage.py" $INNERWORKDIR/gcov $INNERWORKDIR/combined/result
 
 and echo "creating gcno tar"
 and pushd ArangoDB/build
 and tar c -f /tmp/gcno.tar (find . -name "*.gcno")
 and popd
 and echo "copying gcno files"
-and tar x -f /tmp/gcno.tar -C /work/combined/result
+and tar x -f /tmp/gcno.tar -C $INNERWORKDIR/combined/result
 
-and rm -rf coverage
-and mkdir coverage
-and mkdir coverage/enterprise
-and ln -s /work/ArangoDB/3rdParty/jemalloc/v*/include /work/ArangoDB/include
-and gcovr --exclude-throw-branches --root /work/ArangoDB \
+and rm -rf $INNERWORKDIR/coverage
+and mkdir $INNERWORKDIR/coverage
+and mkdir $INNERWORKDIR/coverage/enterprise
+and ln -s $INNERWORKDIR/ArangoDB/3rdParty/jemalloc/v*/include $INNERWORKDIR/ArangoDB/include
+and gcovr --exclude-throw-branches --root $INNERWORKDIR/ArangoDB \
         -x \
-        -e /work/ArangoDB/build \
-        -e /work/ArangoDB/build/3rdParty/libunwind/v* \
-        -e /work/ArangoDB/build/3rdParty/libunwind/v*/src/ \
-        -e /work/ArangoDB/3rdParty \
-        -e /work/ArangoDB/3rdParty/jemalloc/v*/ \
-        -e /work/ArangoDB/usr/ \
-        -e /work/ArangoDB/tests \
+        -e $INNERWORKDIR/ArangoDB/build \
+        -e $INNERWORKDIR/ArangoDB/build/3rdParty/libunwind/v* \
+        -e $INNERWORKDIR/ArangoDB/build/3rdParty/libunwind/v*/src/ \
+        -e $INNERWORKDIR/ArangoDB/3rdParty \
+        -e $INNERWORKDIR/ArangoDB/3rdParty/jemalloc/v*/ \
+        -e $INNERWORKDIR/ArangoDB/usr/ \
+        -e $INNERWORKDIR/ArangoDB/tests \
         -o coverage/coverage.xml \
         --exclude-lines-by-pattern "TRI_ASSERT" \
         --print-summary \
-        /work/combined/result > /work/coverage/summary.txt
+        $INNERWORKDIR/combined/result > $INNERWORKDIR/coverage/summary.txt
 and cat coverage/coverage.xml \
       | sed -e "s:filename=\":filename=\"./coverage/:g" \
-      > coverage/coverage.xml.tmp
-and mv coverage/coverage.xml coverage/coverage.org.xml
-and mv coverage/coverage.xml.tmp coverage/coverage.xml
+      > $INNERWORKDIR/coverage/coverage.xml.tmp
+and mv $INNERWORKDIR/coverage/coverage.xml $INNERWORKDIR/coverage/coverage.org.xml
+and mv $INNERWORKDIR/coverage/coverage.xml.tmp $INNERWORKDIR/coverage/coverage.xml
 and for d in lib arangosh client-tools arangod enterprise/Enterprise
-  if test -d /work/ArangoDB/$d
-  	echo "cp -a /work/ArangoDB/$d coverage/$d"
-  	cp -a /work/ArangoDB/$d coverage/$d
+  if test -d $INNERWORKDIR/ArangoDB/$d
+  	echo "cp -a $INNERWORKDIR/ArangoDB/$d $INNERWORKDIR/coverage/$d"
+  	cp -a $INNERWORKDIR/ArangoDB/$d $INNERWORKDIR/coverage/$d
   end
-and if test -d /work/ArangoDB/enterprise/tests
-        echo "cp -a /work/ArangoDB/enterprise/tests coverage/enterprise/tests"
-        cp -a /work/ArangoDB/enterprise/tests coverage/enterprise/tests
+and if test -d $INNERWORKDIR/ArangoDB/enterprise/tests
+        echo "cp -a $INNERWORKDIR/ArangoDB/enterprise/tests $INNERWORKDIR/coverage/enterprise/tests"
+        cp -a $INNERWORKDIR/ArangoDB/enterprise/tests $INNERWORKDIR/coverage/enterprise/tests
     end
 end
