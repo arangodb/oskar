67355bc7839d8b4641163a9527e4f46de65bd257
Valery Mironov <32071355+MBkkt@users.noreply.github.com>
2023-04-14 23:54:53 +0300

    Remove useless wasteful output

--- jenkins/helper/async_client.py
+++ jenkins/helper/async_client.py
@@ -164,24 +164,7 @@ def convert_result(result_array):
     return result
 
 def add_message_to_report(params, string, print_it = True, add_to_error = False):
-    """ add a message from python to the report strings/files + print it """
-    oskar = 'OSKAR'
-    count = int(80 / len(oskar))
-    datestr = f'  {datetime.now()} - '
-    offset = 80 - (len(string) + len(datestr) + 2 * len(oskar))
-    if print_it:
-        print(string)
-    if add_to_error:
-        params['error'] += 'async_client.py: ' + string + '\n'
-    if isinstance(params['output'], list):
-        params['output'] += \
-            f"{oskar*count}\n{oskar}{datestr}{string}{' '*offset}{oskar}\n{oskar*count}\n"
-    else:
-        params['output'].write(bytearray(
-            f"{oskar*count}\n{oskar}{datestr}{string}{' '*offset}{oskar}\n{oskar*count}\n",
-            "utf-8"))
-        params['output'].flush()
-    sys.stdout.flush()
+    """ return message + \n """
     return string + '\n'
 
 def kill_children(identifier, params, children):
