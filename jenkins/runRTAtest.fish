2266a1e758f4e9c86aecb6d2d49de52831c4255f
Wilfried Goesgens <willi@arangodb.com>
2023-06-13 18:10:58 +0200

    patch it up

--- jenkins/runRTAtest.fish
+++ jenkins/runRTAtest.fish
@@ -13,17 +13,24 @@ and setAllLogsToWorkspace
 and switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH true
 and updateDockerBuildImage
 and pingDetails
+echo "santoeuhsanoteuhxxxx"
 and TT_setup
-and oskarCompile
+# and oskarCompile
 and TT_compile
+echo "santoeuhsanoteuh"
 
-and downloadStarter
-and downloadSyncer
-and copyRclone
-and checkoutRTA
+downloadStarter
+pwd
+cp work/ArangoDB/build/install/usr/bin/arangodb work/ArangoDB/build/bin/
+# and downloadSyncer
+# cp work/ArangoDB/build/install/usr/sbin/arangosync work/ArangoDB/build/bin/
+# and copyRclone
+checkoutRTA
 
 pwd
 cd work/release-test-automation/
+git checkout feature/mixed-source-zip-upgrade
+chmod a+x ./jenkins/oskar_tar.sh
 ./jenkins/oskar_tar.sh
 # cd "$HOME/$NODE_NAME/$OSKAR" ; moveResultsToWorkspace ; unlockDirectory
 exit $s
