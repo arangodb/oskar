e5a4f07ef31c0b5045228ab8addffbfa6b7a88a4
Alexey Bakharew <alex.bakharew@gmail.com>
2023-07-20 18:02:08 +0200

    Some fixes

--- scripts/lib/build.fish
+++ scripts/lib/build.fish
@@ -158,10 +158,8 @@ function shutdownCcache
 end
 
 function selectArchitecture
-  echo "NIGHTLY BUILD1: $BUILD_REPO_INFO"
   if begin test "$USE_ARM" = "On";and test (string match -ir '^arm64$|^aarch64$' "$ARCH"); end
     echo "using architecture ARM"
-    
     set -g FULLARGS $FULLARGS \
       -DCMAKE_SYSTEM_PROCESSOR="aarch64" -DASM_OPTIMIZATIONS=Off
   else
