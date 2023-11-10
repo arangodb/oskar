16726dd8cfbb2bf1dffb731ed64ee0b4e2c5fa96
Max Neunhoeffer <max@arangodb.com>
2023-11-10 10:08:57 +0100

    Add community.

--- containers/buildAlpaquita1.docker/scripts/install-alpine318-repository.sh
+++ containers/buildAlpaquita1.docker/scripts/install-alpine318-repository.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "http://dl-cdn.alpinelinux.org/alpine/v3.18/main" >> /etc/apk/repositories
+echo "http://dl-cdn.alpinelinux.org/alpine/v3.18/community" >> /etc/apk/repositories
 wget -q -O /etc/apk/keys/alpine-devel@lists.alpinelinux.org-4a6a0840.rsa.pub https://raw.githubusercontent.com/alpinelinux/aports/946af5ada94736faeea91d06f3fb685eb6b4adb3/main/alpine-keys/alpine-devel@lists.alpinelinux.org-4a6a0840.rsa.pub
 wget -q -O /etc/apk/keys/alpine-devel@lists.alpinelinux.org-6165ee59.rsa.pub https://raw.githubusercontent.com/alpinelinux/aports/946af5ada94736faeea91d06f3fb685eb6b4adb3/main/alpine-keys/alpine-devel@lists.alpinelinux.org-6165ee59.rsa.pub
 wget -q -O /etc/apk/keys/alpine-devel@lists.alpinelinux.org-5243ef4b.rsa.pub https://raw.githubusercontent.com/alpinelinux/aports/946af5ada94736faeea91d06f3fb685eb6b4adb3/main/alpine-keys/alpine-devel@lists.alpinelinux.org-5243ef4b.rsa.pub
