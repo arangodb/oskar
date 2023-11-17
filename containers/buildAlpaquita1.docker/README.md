197d141188daf1cb7efc36ee99c26808159438e0
Max Neunhoeffer <max@arangodb.com>
2023-11-17 10:08:23 +0100

    Remove Alpaquita try.

--- containers/buildAlpaquita1.docker/README.md
+++
@@ -1,9 +0,0 @@
-# Building the Alpaquita build image
-
-Alpaquita is missing some apk packages which we need.
-
-This Dockerfile will simply grab them from the Alpine 3.18 repositories.
-
-To build this image, just do:
-
-    docker build -t arangodb/alpaquitabuildimage1-x86_64:1 .
--- containers/buildAlpaquita1.docker/scripts/install-alpine318-repository.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "http://dl-cdn.alpinelinux.org/alpine/v3.18/main" >> /etc/apk/repositories
-echo "http://dl-cdn.alpinelinux.org/alpine/v3.18/community" >> /etc/apk/repositories
-wget -q -O /etc/apk/keys/alpine-devel@lists.alpinelinux.org-4a6a0840.rsa.pub https://raw.githubusercontent.com/alpinelinux/aports/946af5ada94736faeea91d06f3fb685eb6b4adb3/main/alpine-keys/alpine-devel@lists.alpinelinux.org-4a6a0840.rsa.pub
-wget -q -O /etc/apk/keys/alpine-devel@lists.alpinelinux.org-6165ee59.rsa.pub https://raw.githubusercontent.com/alpinelinux/aports/946af5ada94736faeea91d06f3fb685eb6b4adb3/main/alpine-keys/alpine-devel@lists.alpinelinux.org-6165ee59.rsa.pub
-wget -q -O /etc/apk/keys/alpine-devel@lists.alpinelinux.org-5243ef4b.rsa.pub https://raw.githubusercontent.com/alpinelinux/aports/946af5ada94736faeea91d06f3fb685eb6b4adb3/main/alpine-keys/alpine-devel@lists.alpinelinux.org-5243ef4b.rsa.pub
-wget -q -O /etc/apk/keys/alpine-devel@lists.alpinelinux.org-61666e3f.rsa.pub https://raw.githubusercontent.com/alpinelinux/aports/946af5ada94736faeea91d06f3fb685eb6b4adb3/main/alpine-keys/alpine-devel@lists.alpinelinux.org-61666e3f.rsa.pub
-wget -q -O /etc/apk/keys/alpine-devel@lists.alpinelinux.org-5261cecb.rsa.pub https://raw.githubusercontent.com/alpinelinux/aports/946af5ada94736faeea91d06f3fb685eb6b4adb3/main/alpine-keys/alpine-devel@lists.alpinelinux.org-5261cecb.rsa.pub
-apk update
