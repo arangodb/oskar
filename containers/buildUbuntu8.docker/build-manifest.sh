837dc51acb4248634f9b7a862ddc53443e30e600
Max Neunhoeffer <max@arangodb.com>
2023-12-12 08:17:09 +0100

    Finishing touches.

--- containers/buildUbuntu8.docker/build-manifest.sh
+++ containers/buildUbuntu8.docker/build-manifest.sh
@@ -1,6 +1,9 @@
 set -e
 
-manifest=arangodb/ubuntubuildarangodb8
+manifest=$1
+IMAGE_TAG=$2
+
+[ -z manifest ] && manifest=arangodb/ubuntubuildarangodb8
 # IMAGE_TAG is set within build-image_*.sh and expected to be equal there
 [ -z "$IMAGE_TAG" ] && IMAGE_TAG=1
 
@@ -8,7 +11,7 @@ echo "Creating docker multiarch manifest \"${manifest}:${IMAGE_TAG}\":"
 set +e; docker manifest rm -f ${manifest}:${IMAGE_TAG} 2>/dev/null; set -e
 docker manifest create ${manifest}:${IMAGE_TAG} \
   --amend ${manifest}:${IMAGE_TAG}-x86_64 \
-  --amend ${manifest}:${IMAGE_TAG}-aarch64 \
+  --amend ${manifest}:${IMAGE_TAG}-amd64 \
 || ( echo "Error during docker multiarch manifest creation!"; exit 1 )
 
 echo "To push the manifest please run:"
