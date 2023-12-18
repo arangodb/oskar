fa259bdf5424162a1f4ded7a9690f6c9284b9056
Max Neunhoeffer <max@arangodb.com>
2023-12-18 15:04:59 +0100

    Rename to ubuntubuildarangodb7 image.

---
+++ containers/buildUbuntu7.docker/build-manifest.sh
@@ -0,0 +1,18 @@
+set -e
+
+manifest=$1
+IMAGE_TAG=$2
+
+[ -z manifest ] && manifest=neunhoef/ubuntubuildarangodb7
+# IMAGE_TAG is set within build-image_*.sh and expected to be equal there
+[ -z "$IMAGE_TAG" ] && IMAGE_TAG=1
+
+echo "Creating docker multiarch manifest \"${manifest}:${IMAGE_TAG}\":"
+set +e; docker manifest rm -f ${manifest}:${IMAGE_TAG} 2>/dev/null; set -e
+docker manifest create ${manifest}:${IMAGE_TAG} \
+  --amend ${manifest}-x86_64:${IMAGE_TAG} \
+  --amend ${manifest}-arm64:${IMAGE_TAG} \
+|| ( echo "Error during docker multiarch manifest creation!"; exit 1 )
+
+echo "To push the manifest please run:"
+echo "  docker manifest push --purge ${manifest}:${IMAGE_TAG}"
--- containers/buildUbuntu7.docker/tools/install.sh
+++ containers/buildUbuntu7.docker/tools/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Set links for GCC
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${COMPILER_VERSION} 10 \
@@ -25,12 +25,14 @@ if [ "$OPENSSLBRANCH" != "3.1" ]; then
 fi;
 
 export OPENSSLPATH=`echo $OPENSSLVERSION | sed 's/\.[0-9]*$//g'`
+[ "$ARCH" = "x86_64" -a ${OPENSSLPATH:0:1} = "3" ] && X86_64_SUFFIX=64
+
 cd /tmp
 curl -O https://www.openssl.org/source/openssl-$OPENSSLVERSION.tar.gz
 tar xzf openssl-$OPENSSLVERSION.tar.gz
 cd openssl-$OPENSSLVERSION
-./config --prefix=/opt/openssl-$OPENSSLPATH no-async no-dso
-make
+./config --prefix=/opt no-async no-dso
+make -j64
 make install_dev
 cd /tmp
 rm -rf openssl-$OPENSSLVERSION.tar.gz openssl-$OPENSSLVERSION
@@ -41,9 +43,9 @@ cd /tmp
 curl -O ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-$OPENLDAPVERSION.tgz
 tar xzf openldap-$OPENLDAPVERSION.tgz
 cd openldap-$OPENLDAPVERSION
-CPPFLAGS=-I/opt/openssl-$OPENSSLPATH/include \
-LDFLAGS=-L/opt/openssl-$OPENSSLPATH/lib64 \
-./configure -prefix=/opt/openssl-$OPENSSLPATH --enable-static
+CPPFLAGS=-I/opt/include \
+LDFLAGS="-L/opt/lib$X86_64_SUFFIX" \
+./configure -prefix=/opt --enable-static
 make depend && make -j64
 make install
 cd /tmp
