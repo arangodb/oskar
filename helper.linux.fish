c3693133172a6b2a640c2b82e7458ba9413e9559
Wilfried Goesgens <willi@arangodb.com>
2024-02-20 15:26:27 +0100

    no stdout buffering

--- helper.linux.fish
+++ helper.linux.fish
@@ -635,7 +635,7 @@ end
 function collectCoverage
   findRequiredCompiler
   and findRequiredOpenSSL
-  and runInContainer --env LLVM_PROFILE_FILE=/work/gcov/  (findStaticBuildImage)  python3 "$WORKSPACE/jenkins/helper/aggregate_coverage.py" "$INNERWORKDIR/" gcov coverage
+  and runInContainer --env LLVM_PROFILE_FILE=/work/gcov/  (findStaticBuildImage)  python3 -u "$WORKSPACE/jenkins/helper/aggregate_coverage.py" "$INNERWORKDIR/" gcov coverage
   return $status
 end
 
