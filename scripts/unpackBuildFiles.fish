ebe51b0e2d7d1708c9033bf2b21450db66a2fa8b
Vadim Kondratev <vadim@arangodb.com>
2024-06-10 22:51:21 +0200

    Fix if statement

--- scripts/unpackBuildFiles.fish
+++ scripts/unpackBuildFiles.fish
@@ -2,8 +2,9 @@
 
 if test "$ARANGODB_VERSION_MAJOR" -eq 3
   if not test "$ARANGODB_VERSION_MINOR" -ge 12; and begin; test "$ARANGODB_VERSION_MINOR" -le 11; and test "$ARANGODB_VERSION_PATCH" -le 9; end
-   echo "Intentionally don't unpack build files for $ARANGODB_VERSION_MAJOR.$ARANGODB_VERSION_MINOR base!"
-  exit 0
+    echo "Intentionally don't unpack build files for $ARANGODB_VERSION_MAJOR.$ARANGODB_VERSION_MINOR base!"
+    exit 0
+  end
 end
 
 set -l BUILD_FILES_ARCHIVE "$argv[1]"
