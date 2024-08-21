2d2678c3cbee7a1e71e87ad4cb44b7ea7681c462
Wilfried Goesgens <willi@arangodb.com>
2024-08-21 10:57:23 +0200

    if we kill remaining children and make the test red also put the reason into testfailures.txt (#593)

--- jenkins/helper/async_client.py
+++ jenkins/helper/async_client.py
@@ -211,7 +211,8 @@ def kill_children(identifier, params, children):
             killed.append(one_child.pid)
             err += add_message_to_report(
                 params,
-                f"{identifier}: killing {one_child.name()} - {str(one_child.pid)}")
+                f"{identifier}: killing {one_child.name()} - {str(one_child.pid)}",
+                True, True)
             one_child.resume()
         except FileNotFoundError:
             pass
