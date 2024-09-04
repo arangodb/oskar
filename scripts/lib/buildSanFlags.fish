f579524e912fc1906e1f41a05e82b3f7d5818ef0
Wilfried Goesgens <willi@arangodb.com>
2024-09-04 15:29:04 +0200

    configure symbolizer client

--- scripts/lib/buildSanFlags.fish
+++ scripts/lib/buildSanFlags.fish
@@ -13,7 +13,7 @@ function buildSanFlags --argument SRCDIR
       switch "$SAN_MODE"
         case "AULSan"
           # address sanitizer
-          set -xg ASAN_OPTIONS "$common_options:log_path=$INNERWORKDIR/aulsan.log:handle_ioctl=true:check_initialization_order=true:detect_container_overflow=true:detect_stack_use_after_return=false:detect_odr_violation=1:strict_init_order=true"
+          set -xg ASAN_OPTIONS "$common_options:log_path=$INNERWORKDIR/aulsan.log:handle_ioctl=true:check_initialization_order=true:detect_container_overflow=true:detect_stack_use_after_return=false:detect_odr_violation=1:strict_init_order=true:external_symbolizer_path=$INNERWORKDIR/ArangoDB/utils/llvm-symbolizer-client.py"
 
           # leak sanitizer
           set -xg LSAN_OPTIONS "$common_options:log_path=$INNERWORKDIR/aulsan.log"
