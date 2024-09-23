937f7a5710d3f41f78a1772b3c4e8ea1d34f511a
Frank Celler <frank@arangodb.com>
2024-09-23 11:12:45 +0200

    missing end

--- helper.fish
+++ helper.fish
@@ -1,11 +1,11 @@
 set -gx KEYNAME     86FEC04D
 set -gx KEYNAME_OLD 115E1684
 
-if test -z "ARANGODB_GIT_HOST"; set -gx ARANGODB_GIT_URL "github.com"
-if test -z "ARANGODB_GIT_ORGA"; set -gx ARANGODB_GIT_ORGA "arangodb"
-if test -z "STARTER_GIT_ORGA"; set -gx STARTER_GIT_ORGA "arangodb-helper"
-if test -z "ENTERPRISE_GIT_HOST"; set -gx ENTERPRISE_GIT_URL "github.com"
-if test -z "ENTERPRISE_GIT_ORGA"; set -gx ENTERPRISE_GIT_ORGA "arangodb"
+if test -z "ARANGODB_GIT_HOST"; set -gx ARANGODB_GIT_URL "github.com"; end
+if test -z "ARANGODB_GIT_ORGA"; set -gx ARANGODB_GIT_ORGA "arangodb"; end
+if test -z "STARTER_GIT_ORGA"; set -gx STARTER_GIT_ORGA "arangodb-helper"; end
+if test -z "ENTERPRISE_GIT_HOST"; set -gx ENTERPRISE_GIT_URL "github.com"; end
+if test -z "ENTERPRISE_GIT_ORGA"; set -gx ENTERPRISE_GIT_ORGA "arangodb"; end
 
 function lockDirectory
   # Now grab the lock ourselves:
