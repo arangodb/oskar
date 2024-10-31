14f42909d6b48f9809977c1db85bacf8e485882f
Wilfried Goesgens <willi@arangodb.com>
2024-10-31 13:01:17 +0100

    backport syntax enhancements

--- jenkins/helper/testing_runner.py
+++ jenkins/helper/testing_runner.py
@@ -108,7 +108,7 @@ def testing_runner(testing_instance, this, arangosh):
         this.delta_seconds = this.delta.total_seconds()
         this.crashed = not this.crashed_file.exists() or this.crashed_file.read_text() == "true"
         this.success = this.success and this.success_file.exists() and this.success_file.read_text() == "true"
-        print(f'done with {this.name_enum} -> {this.success} {ret['rc_exit']}')
+        print(f"done with {this.name_enum} -> {this.success} {ret['rc_exit']}")
         if this.report_file.exists():
             this.structured_results = this.report_file.read_text(encoding="UTF-8", errors='ignore')
         this.summary = ret['error']
