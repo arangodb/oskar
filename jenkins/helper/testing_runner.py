d5753e3d61ae27a94339dc375ea40f64a99f7f64
Wilfried Goesgens <willi@arangodb.com>
2024-11-18 13:08:25 +0100

    write a second jsonl file decyphering logfile -> testing.js PID

--- jenkins/helper/testing_runner.py
+++ jenkins/helper/testing_runner.py
@@ -168,6 +168,8 @@ def testing_runner(testing_instance, this, arangosh):
             print(traceback.format_exc())
             raise ex
         with arangosh.slot_lock:
+            with open((sitecfg.run_root / "job_to_pids.jsonl"), "w+", encoding="utf-8")  as jsonl_file:
+                jsonl_file.write(f'{json.dumps({"pid": ret["pid"], "logfile": this.log_file})}\n')
             testing_instance.running_suites.remove(this.name_enum)
         testing_instance.done_job(this.parallelity)
 
