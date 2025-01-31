cc8be00e42ac2260c8991bd5041fde78e0ceda64
Vadim Kondratev <vadim@arangodb.com>
2025-01-31 15:29:23 +0100

    Finalize changes

--- containers/buildUbuntu311.docker/latest-tag.sh
+++ containers/buildUbuntu311.docker/latest-tag.sh
@@ -1,4 +1,4 @@
-repo="arangodb/ubuntubuildarangodb-311"
+repo="arangodb/ubuntubuildarangodb-devel"
 token=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${repo}:pull" | jq -r '.token')
 latest=$(curl -H "Authorization: Bearer $token" -s "https://registry-1.docker.io/v2/${repo}/tags/list" | jq -r '.tags | values[]' | grep -e "^[0-9]*$" | sort -n | tail -1)
 
