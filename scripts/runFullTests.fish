cd577f059bd7a26c1a4dbafbdd862234fef68818
Vadim Kondratev <vadim@arangodb.com>
2025-03-05 13:21:28 +0300

    Update runFullTests.fish

--- scripts/runFullTests.fish
+++ scripts/runFullTests.fish
@@ -101,7 +101,7 @@ switch $TESTSUITE
     set suiteRunner "launchClusterTests"
   case "single_cluster"
     resetLaunch 4
-    set -xg timeLimit 70000
+    set -xg timeLimit 80000
     set suiteRunner "launchSingleClusterTests"
   case "single"
     resetLaunch 1
