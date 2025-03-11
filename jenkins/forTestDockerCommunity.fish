3dfcdd1bf3cd14bd46b62db146ae455f745d09bb
Wilfried Goesgens <willi@arangodb.com>
2025-03-11 15:39:46 +0100

    work on using podman instead of docker

--- jenkins/forTestDockerCommunity.fish
+++ jenkins/forTestDockerCommunity.fish
@@ -39,9 +39,9 @@ and setArchSuffix
 and set -xg HUB_COMMUNITY "arangodb/arangodb-test:$DOCKER_TAG_JENKINS$archSuffix"
 and buildDockerImage $HUB_COMMUNITY
 and validateDockerImageIfNeeded $HUB_COMMUNITY
-and docker push $HUB_COMMUNITY
-and docker tag $HUB_COMMUNITY $GCR_REG_PREFIX$HUB_COMMUNITY
-and docker push $GCR_REG_PREFIX$HUB_COMMUNITY
+and "$DOCKER" push $HUB_COMMUNITY
+and "$DOCKER" tag $HUB_COMMUNITY $GCR_REG_PREFIX$HUB_COMMUNITY
+and "$DOCKER" push $GCR_REG_PREFIX$HUB_COMMUNITY
 and echo $HUB_COMMUNITY >> $WORKSPACE/imagenames.log
 and echo $GCR_REG_PREFIX$HUB_COMMUNITY >> $WORKSPACE/imagenames.log
 
