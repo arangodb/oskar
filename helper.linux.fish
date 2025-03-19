2565b3cfd6d9fc5ae43442c85a037d8646308f3d
Vitaly Pryakhin <vitaly@arangodb.com>
2025-03-19 19:00:42 +0200

    WIP

--- helper.linux.fish
+++ helper.linux.fish
@@ -1252,13 +1252,14 @@ function validateDockerImageIfNeeded
     return 1
   end
   set -l image_name $argv[1]
+  set -l filesafe_image_name (string replace "/" "-" -- $image_name)
   if test "$RUN_CVE_CHECKS_FOR_DOCKER_IMAGE" = "1"; or test "$RUN_CVE_CHECKS_FOR_DOCKER_IMAGE" = "On"
     if test "$CREATE_CVE_REPORT_FOR_DOCKER_IMAGE" = "1"; or test "$CREATE_CVE_REPORT_FOR_DOCKER_IMAGE" = "On"
       set -l grype_report_dir $WORKDIR/work/grype_reports
       if ! test -d $grype_report_dir
         mkdir -p $grype_report_dir
       end
-      set -l CVE_REPORT_FILE $grype_report_dir/grype-cve-report-$image_name.txt
+      set -l CVE_REPORT_FILE $grype_report_dir/grype-cve-report-$filesafe_image_name.txt
       checkDockerImageForCves $image_name $CVE_REPORT_FILE
     else
       checkDockerImageForCves $image_name
