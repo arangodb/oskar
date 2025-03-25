89315c62e87d174dc6cda38772d10625423c9e90
Wilfried Goesgens <willi@arangodb.com>
2025-03-25 12:39:23 +0100

    only zip if we have indicators of a crash.

--- jenkins/helper/testing_runner.py
+++ jenkins/helper/testing_runner.py
@@ -524,9 +524,10 @@ class TestingRunner():
             shutil.rmtree(str(core_zip_dir), ignore_errors=True)
 
             self.cleanup_unneeded_binary_files()
-        binary_report_file = get_workspace() / datetime.now(tz=None).strftime(f"binaries-{self.cfg.datetime_format}")
-        bin_files_list = [f for f in self.cfg.bin_dir.glob('*') if not f.is_symlink()]
-        self.mp_zip_tar(bin_files_list, self.cfg.bin_dir, binary_report_file, 'binary support', 'binreport')
+        if self.crashed:
+            binary_report_file = get_workspace() / datetime.now(tz=None).strftime(f"binaries-{self.cfg.datetime_format}")
+            bin_files_list = [f for f in self.cfg.bin_dir.glob('*') if not f.is_symlink()]
+            self.mp_zip_tar(bin_files_list, self.cfg.bin_dir, binary_report_file, 'binary support', 'binreport')
 
     def generate_test_report(self):
         """ regular testresults zip """
