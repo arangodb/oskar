1e068f264f9fc0e809966f30218a95d6a1a6e04b
Wilfried Goesgens <willi@arangodb.com>
2025-04-04 15:52:29 +0200

    use default container args

--- helper.linux.fish
+++ helper.linux.fish
@@ -1699,8 +1699,9 @@ function runInContainer
   # from a regular user. Therefore we have to do some Eiertanz to stop it
   # if we receive a TERM outside the container. Note that this does not
   # cover SIGINT, since this will directly abort the whole function.
+  set -l fish_trace on
   set c ("$DOCKER" run -d --cap-add=SYS_PTRACE --privileged --security-opt seccomp=unconfined \
-             --pids-limit 256704 \
+             "${DEFAULT_DOCKER_ARGS}" \
              -m 64G \
              -v $WORKDIR/work/:$INNERWORKDIR \
              -v $SSH_AUTH_SOCK:/ssh-agent \
