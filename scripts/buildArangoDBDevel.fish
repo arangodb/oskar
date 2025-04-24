ca17e6543715a1ea4a9d996f184b5b018051c19e
Wilfried Goesgens <willi@arangodb.com>
2025-04-24 14:28:08 +0200

    skip this subdir, its gone for no occurant reason

--- scripts/buildArangoDBDevel.fish
+++ scripts/buildArangoDBDevel.fish
@@ -39,10 +39,10 @@ end
 
 if test "$ARCH" = "x86_64"
   set -xg LAPACK_LIB_PATH "/usr/lib/x86_64-linux-gnu/lapack/liblapack.a"
-  set -xg BLAS_LIB_PATH "/usr/lib/x86_64-linux-gnu/blas/libblas.a"
+  set -xg BLAS_LIB_PATH "/usr/lib/x86_64-linux-gnu/libblas.a"
 else
   set -xg LAPACK_LIB_PATH "/usr/lib/aarch64-linux-gnu/lapack/liblapack.a"
-  set -xg BLAS_LIB_PATH "/usr/lib/aarch64-linux-gnu/blas/libblas.a"
+  set -xg BLAS_LIB_PATH "/usr/lib/aarch64-linux-gnu/libblas.a"
 end
 
 set -l pie ""
