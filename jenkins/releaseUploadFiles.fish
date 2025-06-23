d72645fd06b2431d000d7287804cb4df66ed5b96
Vadim Kondratev <vadim@arangodb.com>
2025-06-23 12:38:22 +0200

    Publish only EE-related entities for 3.12.5+

--- jenkins/releaseUploadFiles.fish
+++ jenkins/releaseUploadFiles.fish
@@ -19,11 +19,18 @@ or begin unlockDirectory ; exit 1 ; end
 
 function upload
   cd /mnt/buildfiles/stage2
-  and echo "Copying COMMUNITY"
+  and if test "$ARANGODB_VERSION_MAJOR" -eq 3
+        if test "$ARANGODB_VERSION_MINOR" -le 11; or begin; test "$ARANGODB_VERSION_MINOR" -eq 12; and test "$ARANGODB_VERSION_PATCH" -lt 5; end
+          echo "Copying COMMUNITY"
+          and gsutil -m rsync -c -r $ARANGODB_PACKAGES/packages/Community gs://download.arangodb.com/$ARANGODB_REPO/Community
+          and gsutil -m rsync -c -r $ARANGODB_PACKAGES/repositories/Community/Debian gs://download.arangodb.com/$ARANGODB_REPO/DEBIAN
+          and gsutil -m rsync -c -r $ARANGODB_PACKAGES/repositories/Community/RPM gs://download.arangodb.com/$ARANGODB_REPO/RPM
+        else
+          echo "Copying only COMMUNITY (part) SOURCE for 3.12.5+ EE!"
+        end
+      end
+  and gsutil -m rsync -c -x 'index\.html' -r $ARANGODB_PACKAGES/source gs://download.arangodb.com/Source
   and gsutil -m rsync -c -r $ARANGODB_PACKAGES/source gs://download.arangodb.com/Source
-  and gsutil -m rsync -c -r $ARANGODB_PACKAGES/packages/Community gs://download.arangodb.com/$ARANGODB_REPO/Community
-  and gsutil -m rsync -c -r $ARANGODB_PACKAGES/repositories/Community/Debian gs://download.arangodb.com/$ARANGODB_REPO/DEBIAN
-  and gsutil -m rsync -c -r $ARANGODB_PACKAGES/repositories/Community/RPM gs://download.arangodb.com/$ARANGODB_REPO/RPM
   and gsutil cp $ARANGODB_PACKAGES/index.html gs://download.arangodb.com/$ARANGODB_REPO/
   and echo "Copying ENTERPRISE"
   and gsutil -m rsync -c -r $ARANGODB_PACKAGES/packages/Enterprise gs://download.arangodb.com/$ENTERPRISE_DOWNLOAD_KEY/$ARANGODB_REPO/Enterprise
