434373d828e575996cc4cd5e0bae29b455d42a38
Wilfried Goesgens <willi@arangodb.com>
2025-10-13 14:41:00 +0200

    backport yaml parsing

--- jenkins/helper/test_launch_controller.py
+++ jenkins/helper/test_launch_controller.py
@@ -3,7 +3,9 @@
 import argparse
 import copy
 import sys
+import json
 from traceback import print_exc
+import yaml
 
 from site_config import IS_ARM, IS_WINDOWS, IS_MAC, IS_COVERAGE
 
@@ -110,7 +112,10 @@ known_parameter = {
     "buckets": "number of buckets to use for this test",
     "suffix": "suffix that is appended to the tests folder name",
     "priority": "priority that controls execution order. Testsuites with lower priority are executed later",
-    "parallelity": "parallelity how many resources will the job use in the SUT? Default: 1 in Single server, 4 in Clusters"
+    "parallelity": "parallelity how many resources will the job use in the SUT? Default: 1 in Single server, 4 in Clusters",
+    "type": "single or cluster flag",
+    "full": "whether to spare from a single or full run",
+    "sniff": "whether to enable sniffing",
 }
 
 
@@ -232,22 +237,149 @@ def read_definition_line(line):
         "params": params
     }
 
+def read_yaml_suite(name, suite, definition, testfile_definitions, bucket_name, yaml_struct):
+    """ convert yaml representation into the internal one """
+    if not 'options' in definition:
+        definition['options'] = {}
+    flags = []
+    params = {}
+    arangosh_args = []
+    args = []
+    if 'args' in definition:
+        if not isinstance(definition['args'], dict):
+            raise Exception(f"expected args to be a key value list! have: {definition['args']}")
+        for key, val in definition['args'].items():
+            if key == 'moreArgv':
+                args.append(val)
+            else:
+                args.append(f"--{key}")
+                if isinstance(val, bool):
+                    args.append("true" if val else "false")
+                else:
+                    args.append(val)
+    if 'arangosh_args' in definition:
+        if not isinstance(definition['arangosh_args'], dict):
+            raise Exception(f"expected arangosh_args to be a key value list! have: {definition['arangosh_args']}")
+        for key, val in definition['arangosh_args'].items():
+            arangosh_args.append(f"--{key}")
+            if isinstance(val, bool):
+                arangosh_args.append("true" if val else "false")
+            else:
+                arangosh_args.append(val)
+
+    is_cluster = False
+    if 'type' in params:
+        if params['type'] == "cluster":
+            is_cluster = True
+            flags.append('cluster')
+        else:
+            flags.append('single')
+    size = "medium" if is_cluster else "small"
+    size = size if not "size" in params else params['size']
+    params = validate_params(definition['options'], is_cluster)
+
+    if 'full' in params:
+        flags.append("full" if params["full"] else "!full")
+    if 'coverage' in params:
+        flags.append("coverage" if params["coverage"] else "!coverage")
+    if 'sniff' in params:
+        flags.append("sniff" if params["sniff"] else "!sniff")
+    if yaml_struct != {}:
+        run_job = yaml_struct['add-yaml']['derives-to']
+    else:
+        run_job = 'run-linux-tests'
+    return {
+        "bucket": bucket_name,
+        "name": name if not "name" in params else params['name'],
+        "suites": suite,
+        "size": size,
+        "flags": flags,
+        "args": args.copy(),
+        "arangosh_args": arangosh_args.copy(),
+        "params": params.copy(),
+        "testfile_definitions": testfile_definitions,
+        "run_job": run_job,
+    }
+
+def read_yaml_serial_suite(name, definition, testfile_definitions, bucket_name, yaml_struct):
+    """ convert yaml representation into the internal one """
+    generated_definition = {
+    }
+    if 'options' in definition:
+        generated_definition['options'] = definition['options']
+    args = {}
+    generated_name = ""
+    if 'args' in definition:
+        args = definition['args'].copy()
+    if isinstance(definition['suites'][0], str):
+        generated_name = ','.join(definition['suites'])
+    else:
+        suite_strs = []
+        options_json = []
+        for suite in definition['suites']:
+            suite_name = list(suite.keys())[0]
+            if 'args' in suite[suite_name]:
+                options_json.append(suite[suite_name]['args'])
+            suite_strs.append(suite_name)
+        generated_name = ','.join(suite_strs)
+        args['optionsJson'] = json.dumps(options_json, separators=(',', ':'))
+    if args != {}:
+        generated_definition['args'] = args
+    name = generated_name
+    if 'name' in definition:
+        name = definition['name']
+    return read_yaml_suite(name, generated_name, generated_definition, testfile_definitions, bucket_name, yaml_struct)
+
+def read_yaml_bucket_suite(name, definition, testfile_definitions, bucket_name, yaml_struct):
+    """ convert yaml representation into the internal one """
+    bucket_name = definition['name']
+    ret = []
+    for suite in definition['suites']:
+        suite_name = list(suite.keys())[0]
+        ret.append(read_yaml_suite(suite_name, suite_name, suite[suite_name], testfile_definitions, bucket_name, yaml_struct))
+    return ret
 
 def read_definitions(filename):
     """ read test definitions txt """
     tests = []
     has_error = False
-    with open(filename, "r", encoding="utf-8") as filep:
-        for line_no, line in enumerate(filep):
-            line = line.strip()
-            if line.startswith("#") or len(line) == 0:
-                continue  # ignore comments
-            try:
-                test = read_definition_line(line)
-                tests.append(test)
-            except Exception as exc:
-                print(f"{filename}:{line_no + 1}: \n`{line}`\n {exc}", file=sys.stderr)
-                has_error = True
+    testfile_definitions = {}
+    yaml_text = ""
+    have_yaml = False
+    parsed_yaml = {}
+    if filename.endswith(".yml"):
+        with open(filename, "r", encoding="utf-8") as filep:
+            config = yaml.safe_load(filep)
+            filtered_config = []
+            for testcase in config:
+                suite_name = list(testcase.keys())[0]
+                if suite_name == "add-yaml":
+                    parsed_yaml = {"add-yaml": copy.deepcopy(testcase["add-yaml"])}
+                elif suite_name == "jobProperties":
+                    testfile_definitions = copy.deepcopy(testcase["jobProperties"])
+                else:
+                    filtered_config.append(testcase)
+            for testcase in filtered_config:
+                suite_name = list(testcase.keys())[0]
+                if suite_name == "serial":
+                    tests.append(read_yaml_serial_suite(suite_name, testcase, testfile_definitions, None, parsed_yaml))
+                elif suite_name == "bucket":
+                    tests += read_yaml_bucket_suite(suite_name, testcase, testfile_definitions, None, parsed_yaml)
+                    None
+                else:
+                    tests.append(read_yaml_suite(suite_name, suite_name, testcase[suite_name], testfile_definitions, None, parsed_yaml))
+    else:
+        with open(filename, "r", encoding="utf-8") as filep:
+            for line_no, line in enumerate(filep):
+                line = line.strip()
+                if line.startswith("#") or len(line) == 0:
+                    continue  # ignore comments
+                try:
+                    test = read_definition_line(line)
+                    tests.append(test)
+                except Exception as exc:
+                    print(f"{filename}:{line_no + 1}: \n`{line}`\n {exc}", file=sys.stderr)
+                    has_error = True
     if has_error:
         raise Exception("abort due to errors")
     return tests
