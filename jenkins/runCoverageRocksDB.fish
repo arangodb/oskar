886687c7650a74138ae4627be570c691d60ba359
Wilfried Goesgens <willi@arangodb.com>
2025-11-11 09:57:51 +0100

    we run outside the docker container, need different variable

--- jenkins/runCoverageRocksDB.fish
+++ jenkins/runCoverageRocksDB.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-if test -f "$INNERWORKDIR/ArangoDB/tests/test-definitions.yml"
+if test -f "$WORKDIR/ArangoDB/tests/test-definitions.yml"
    set -xg TEST_DEFINITIONS test-definitions.yml
 else
    set -xg TEST_DEFINITIONS test-definitions.txt
