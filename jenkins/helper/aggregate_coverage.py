7ba361c22f42b6ac3b0ca0ae3d2f50bfbd0798f9
Wilfried Goesgens <willi@arangodb.com>
2026-08-18 10:06:43 +0200

    print file size generated

--- jenkins/helper/aggregate_coverage.py
+++ jenkins/helper/aggregate_coverage.py
@@ -74,7 +74,7 @@ class LlvmCov(ArangoCLIprogressiveTimeoutExecutor):
             {"".join(traceback.TracebackException.from_exception(ex).format())}''')
             self.params['error'] += str(ex)
         end = datetime.now()
-        print(f'done with {binary} in {end-start} {str(coverage_file)} {coverage_file.stat().st_size}} => {str(lcov_file)} {lcov_file.stat().st_size}')
+        print(f'done with {binary} in {end-start} {str(coverage_file)} {coverage_file.stat().st_size} => {str(lcov_file)} {lcov_file.stat().st_size}')
         ret = {}
         ret['error'] = self.params['error']
         return ret
